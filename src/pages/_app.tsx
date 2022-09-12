e2ab67aecc58f584842e09892f89ba86323d1394
David Taing <adavidtaing@gmail.com>
2022-09-13 06:04:04 +1000

    Add .env.development defaults and move Stripe PK

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,7 @@ Router.events.on("routeChangeStart", () => Nprogress.start());
 Router.events.on("routeChangeComplete", () => Nprogress.done());
 Router.events.on("routeChangeError", () => Nprogress.done());
 
-const stripePromise = loadStripe(
-  "pk_test_51IyV4OFAD84FX6wfbxzuc1uNnh41w0BfoHKLumVTzCXzUSxFG6S5fly3Ry1Xl0kmpb8JoJD5IriwjC0cDO2Zf49W00NE4Gmy9S "
-);
-console.log(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK as string);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
