afcf3d1d2e480fcae27de3c3817390936505cda1
David Taing <adavidtaing@gmail.com>
2022-09-14 10:54:59 +1000

    Move /test-onboard page to /signup
    Moves the new consolidated Customer Signup form to the the /signup route.
    
    fixes #71

---
+++ src/pages/signup.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { CustomerSignupPage } from "../features/CustomerSignupPage";
+
+const SignupPage: NextPage = () => {
+  return <CustomerSignupPage />;
+};
+
+export default SignupPage;
--- src/pages/test-onboard.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import { CustomerSignupPage } from "../features/CustomerSignupPage/";
-
-const TestOnboard: NextPage = () => {
-  return <CustomerSignupPage />;
-};
-
-export default TestOnboard;
