c0f492319eebb7036e0398a2360d558bd6a62a59
davidtaing <adavidtaing@gmail.com>
2022-11-07 13:10:55 +1100

    refactor: remove CustomerOnboard

--- src/pages/account/customer/onboard.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from "next";
-import CustomerOnboard from "../../../components/account/CustomerOnboard";
-
-const CustomerOnboardPage: NextPage = () => {
-  return <CustomerOnboard />;
-};
-
-export default CustomerOnboardPage;
