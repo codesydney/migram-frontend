08215dd664d50ff0730b64fd75b47839f8c32640
David Taing <adavidtaing@gmail.com>
2022-11-20 03:15:39 +1100

    style: override typography
    Styling for the typography is all over the place. Copying the styles
    from the Polaris Design System for now.

--- src/features/Checkout/CheckoutPage.tsx
+++ src/features/Checkout/CheckoutPage.tsx
@@ -4,10 +4,97 @@ import { CheckoutForm } from "./CheckoutForm";
 import { useTaskFetch } from "./hooks";
 
 const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+
   form {
     display: flex;
     flex-direction: column;
     gap: 1rem;
+  }
+
+  fieldset {
+    margin: 0;
+    padding: 0;
+    border: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: 600;
+    margin: 0;
+  }
+
+  h1 {
+    font-size: 2rem;
+    line-height: 2.5rem;
+    margin-bottom: 2rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    line-height: 1.75rem;
+    margin-bottom: 1rem;
+  }
+
+  h3,
+  h4 {
+    font-size: 1.25rem;
+    line-height: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  h5 {
+    font-size: 1rem;
+    line-height: 1.25rem;
+    margin-bottom: 1rem;
+  }
+
+  p,
+  span {
+    font-size: 1rem;
+    line-height: 1.2rem;
+    margin: 0 0 1rem;
+  }
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+
+    h1 {
+      font-size: 2.5rem;
+      line-height: 3rem;
+      margin-bottom: 2.5rem;
+    }
+
+    h2 {
+      font-size: 2rem;
+      line-height: 2.5rem;
+      margin-bottom: 2rem;
+    }
+
+    h3 {
+      font-size: 1.75rem;
+      line-height: 2rem;
+      margin-bottom: 1.75rem;
+    }
+
+    h4 {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+      margin-bottom: 1.25rem;
+    }
+
+    h5 {
+      font-size: 1.25rem;
+      line-height: 1.5rem;
+      margin-bottom: 1.25rem;
+    }
   }
 `;
 
