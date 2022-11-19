2493849209556aae42d4d7a9cfcba5a6480126d3
davidtaing <adavidtaing@gmail.com>
2022-11-19 23:31:03 +1100

    refactor: tests
    - flattened describe -> test blocks into it blocks
    - renamed test names for better clarity
    - adjusted some expect statements for better clarity

--- src/features/Checkout/tests/CheckoutForm.test.tsx
+++ src/features/Checkout/tests/CheckoutForm.test.tsx
@@ -5,33 +5,29 @@ import { CheckoutForm } from "../CheckoutForm";
 jest.mock("next/router", () => require("next-router-mock"));
 
 describe("CheckoutForm", () => {
-  describe("onSubmit", () => {
-    test("it validates billing details", async () => {
-      const user = userEvent.setup();
-      const container = render(<CheckoutForm />);
+  it("should validate billing details onSubmit", async () => {
+    const user = userEvent.setup();
+    const container = render(<CheckoutForm />);
 
-      const checkoutButton = container.getByText(/checkout/i, {
-        selector: "button",
-      });
-
-      await act(async () => user.click(checkoutButton));
-
-      expect(container.getByText(/please enter address line 1/i)).toBeTruthy();
+    const checkoutButton = container.getByText(/checkout/i, {
+      selector: "button",
     });
+
+    await act(async () => user.click(checkoutButton));
+
+    expect(container.getByText(/please enter address line 1/i)).toBeTruthy();
   });
 
-  describe("onBlur", () => {
-    test("it validates billing details", async () => {
-      const user = userEvent.setup();
-      const container = render(<CheckoutForm />);
+  it("should validate billing details, onBlur", async () => {
+    const user = userEvent.setup();
+    const container = render(<CheckoutForm />);
 
-      const nameInput = container.getByLabelText(/name/i);
-      const line1Input = container.getByLabelText(/address line 1/i);
+    const nameInput = container.getByLabelText(/name/i);
+    const line1Input = container.getByLabelText(/address line 1/i);
 
-      await act(async () => user.click(nameInput));
-      await act(async () => user.click(line1Input));
+    await act(async () => user.click(nameInput));
+    await act(async () => user.click(line1Input));
 
-      expect(container.getByText(/please enter a name/i)).toBeTruthy();
-    });
+    expect(container.getByText(/please enter a name/i)).toBeTruthy();
   });
 });
--- src/features/Checkout/tests/CheckoutPage.test.tsx
+++ src/features/Checkout/tests/CheckoutPage.test.tsx
@@ -6,40 +6,36 @@ import { CheckoutPage } from "../CheckoutPage";
 import { QueryClientWrapper } from "../../../test/utils";
 
 describe("CheckoutPage at /checkout/:taskId", () => {
-  describe("When the task is loading", () => {
-    test("The form is disabled", async () => {
-      const user = userEvent.setup();
-      const container = render(<CheckoutPage taskId="1" />, {
-        wrapper: QueryClientWrapper,
-      });
-
-      expect(container.getByText(/loading/i)).toBeTruthy();
-
-      const nameInput = container.getByLabelText(/name/i);
-      const addressLine1Input = container.getByLabelText(/address line 1/i);
-
-      await user.click(nameInput);
-      await user.click(addressLine1Input);
-
-      expect(container.queryByText(/please enter a name/i)).toBeNull();
+  it("The form is disabled when the task is loading", async () => {
+    const user = userEvent.setup();
+    const container = render(<CheckoutPage taskId="1" />, {
+      wrapper: QueryClientWrapper,
     });
+
+    expect(container.getByText(/loading/i)).toBeTruthy();
+
+    const nameInput = container.getByLabelText(/name/i);
+    const addressLine1Input = container.getByLabelText(/address line 1/i);
+
+    await user.click(nameInput);
+    await user.click(addressLine1Input);
+
+    expect(container.queryByText(/please enter a name/i)).toBeNull();
   });
 
-  describe("When the task finishes loading", () => {
-    test("The form is enabled", async () => {
-      const user = userEvent.setup();
-      const container = render(<CheckoutPage taskId="1" />, {
-        wrapper: QueryClientWrapper,
-      });
-
-      await waitFor(() => container.getByText(/task/i));
-
-      const nameInput = container.getByLabelText(/name/i);
-
-      await user.click(nameInput);
-      await userEvent.type(nameInput, "testinput");
-
-      expect(container.getByDisplayValue(/testinput/i)).toBeTruthy();
+  it("The form is enabled when the task finishes loading", async () => {
+    const user = userEvent.setup();
+    const container = render(<CheckoutPage taskId="1" />, {
+      wrapper: QueryClientWrapper,
     });
+
+    await waitFor(() => container.getByText(/task/i));
+
+    const nameInput = container.getByLabelText(/name/i);
+
+    await user.click(nameInput);
+    await userEvent.type(nameInput, "testinput");
+
+    expect(container.getByDisplayValue(/testinput/i)).toBeTruthy();
   });
 });
