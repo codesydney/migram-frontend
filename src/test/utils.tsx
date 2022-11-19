4f9a251083c837e0181f8b4a85037a7677eb005a
davidtaing <adavidtaing@gmail.com>
2022-11-19 23:20:08 +1100

    refactor: disable retries for test queryClient

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,7 +1,13 @@
 import { PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 
 export const QueryClientWrapper = ({ children }: PropsWithChildren<{}>) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
