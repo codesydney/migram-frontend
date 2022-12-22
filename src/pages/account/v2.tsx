249f3fc46d380b14b339b894aeeb05749fcef97c
David Taing <adavidtaing@gmail.com>
2022-12-22 15:23:38 +1100

    feat: Stripe Customer Portal Integration (#115)
    feat: Stripe Customer Portal Integration
    
    - add ManageBillingButton to allow users to navigate to the Stripe Customer Portal
    - add `account/v2` page in preparation for the AccountPage rewrite.

---
+++ src/pages/account/v2.tsx
@@ -0,0 +1,26 @@
+import { User } from "next-auth";
+import { useSession } from "next-auth/react";
+import { FeatureFlag } from "../../utils/FeatureFlag";
+import { ManageBillingButton } from "../../features/Accounts";
+
+type AccountPageV2Props = {
+  user: User | undefined;
+};
+
+export const AccountPageV2 = ({ user }: AccountPageV2Props) => {
+  const customerId = user?.customerId;
+
+  return (
+    <div aria-label="Account Page">
+      {customerId ? <ManageBillingButton customerId={customerId} /> : null}
+    </div>
+  );
+};
+
+const AccountRouteV2 = () => {
+  const { data: session } = useSession();
+
+  return <AccountPageV2 user={session?.user} />;
+};
+
+export default FeatureFlag(AccountRouteV2, { isPage: true });
