f37a425d9e3cb15f23d0a8e42172d1ebe378b57a
davidtaing <adavidtaing@gmail.com>
2022-12-22 13:44:02 +1100

    refactor: split AccountPageV2
    to AccountRouteV2 and AccountPageV2 components

--- src/pages/account/v2.tsx
+++ src/pages/account/v2.tsx
@@ -1,7 +1,18 @@
+import { Session } from "next-auth";
+import { useSession } from "next-auth/react";
 import { FeatureFlag } from "../../utils/FeatureFlag";
 
-const AccountPageV2 = () => {
+const AccountRouteV2 = () => {
+  const { data: session } = useSession();
+  return <AccountPageV2 session={session} />;
+};
+
+type AccountPageV2Props = {
+  session: Session | null | undefined;
+};
+
+export const AccountPageV2 = ({ session }: AccountPageV2Props) => {
   return <div aria-label="Account Page"></div>;
 };
 
-export default FeatureFlag(AccountPageV2, { isPage: true });
+export default FeatureFlag(AccountRouteV2, { isPage: true });
--- src/test/pages/account/v2.test.tsx
+++ src/test/pages/account/v2.test.tsx
@@ -1,10 +1,9 @@
 import { render, screen } from "@testing-library/react";
-
-import AccountPageV2 from "../../../pages/account/v2";
+import { AccountPageV2 } from "../../../pages/account/v2";
 
 describe("AccountPage V2", () => {
-  test("smoke test if it renders", () => {
-    render(<AccountPageV2 />);
+  test("smoke test if it renders", async () => {
+    render(<AccountPageV2 session={undefined} />);
 
     expect(screen.queryByLabelText(/^Account Page$/i)).toBeTruthy();
   });
