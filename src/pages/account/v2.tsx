1aac48c75dd9dc7962560ebc4f47f3e1d57e0712
davidtaing <adavidtaing@gmail.com>
2022-12-22 13:59:50 +1100

    feat(AccountPageV2): add conditional rendering
    based on User type
    
    Other Changes:
    - Convert session prop to user prop
    - Add two tests for conditional rendering logic
    
    chore: reorder components for better clarity

--- src/pages/account/v2.tsx
+++ src/pages/account/v2.tsx
@@ -1,18 +1,26 @@
-import { Session } from "next-auth";
+import { User } from "next-auth";
 import { useSession } from "next-auth/react";
 import { FeatureFlag } from "../../utils/FeatureFlag";
+import { ManageBillingButton } from "../../features/Accounts";
+
+type AccountPageV2Props = {
+  user: User | undefined;
+};
+
+export const AccountPageV2 = ({ user }: AccountPageV2Props) => {
+  const customerId = user?.customerId;
+
+  return (
+    <div aria-label="Account Page">
+      {customerId ? <ManageBillingButton customerId={customerId} /> : null}
+    </div>
+  );
+};
 
 const AccountRouteV2 = () => {
   const { data: session } = useSession();
-  return <AccountPageV2 session={session} />;
-};
 
-type AccountPageV2Props = {
-  session: Session | null | undefined;
-};
-
-export const AccountPageV2 = ({ session }: AccountPageV2Props) => {
-  return <div aria-label="Account Page"></div>;
+  return <AccountPageV2 user={session?.user} />;
 };
 
 export default FeatureFlag(AccountRouteV2, { isPage: true });
--- src/test/pages/account/v2.test.tsx
+++ src/test/pages/account/v2.test.tsx
@@ -1,10 +1,47 @@
 import { render, screen } from "@testing-library/react";
+import { User } from "next-auth";
 import { AccountPageV2 } from "../../../pages/account/v2";
 
 describe("AccountPage V2", () => {
   test("smoke test if it renders", async () => {
-    render(<AccountPageV2 session={undefined} />);
+    render(<AccountPageV2 user={undefined} />);
 
     expect(screen.queryByLabelText(/^Account Page$/i)).toBeTruthy();
+  });
+
+  describe("When the User is a Customer", () => {
+    it("renders ManageBillingButton", () => {
+      const user: User = {
+        id: "1",
+        firstName: "David",
+        lastName: "Taing",
+        email: "customer@email.com",
+        customerId: "cus_MtwlzVhYNsrTSg",
+      };
+
+      render(<AccountPageV2 user={user} />);
+
+      expect(
+        screen.queryByRole("button", { name: /^manage billing$/i })
+      ).toBeTruthy();
+    });
+  });
+
+  describe("When the User is a Provider", () => {
+    it("does not render ManageBillingButton", () => {
+      const user: User = {
+        id: "1",
+        firstName: "David",
+        lastName: "Taing",
+        email: "provider@email.com",
+        providerId: "acct_1Lur4rIWxYvLVjGY",
+      };
+
+      render(<AccountPageV2 user={user} />);
+
+      expect(
+        screen.queryByRole("button", { name: /^manage billing$/i })
+      ).toBeNull();
+    });
   });
 });
