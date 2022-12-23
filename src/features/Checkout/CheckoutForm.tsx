0330ad95b8fb24eb7aa4570af6b46d23ca82570b
davidtaing <adavidtaing@gmail.com>
2022-12-23 14:22:32 +1100

    refactor: CardElement to separate file

--- src/features/Checkout/CheckoutForm.tsx
+++ src/features/Checkout/CheckoutForm.tsx
@@ -2,39 +2,19 @@
 import ButtonStyles from "../../components/styles/ButtonStyles";
 import {
   AddressElement,
-  CardElement,
+  CardElement as CardElementPrimitive,
   useElements,
   useStripe,
 } from "@stripe/react-stripe-js";
-import styled from "styled-components";
 import React, { useState } from "react";
 import { useCreatePaymentIntent } from "./hooks";
 import { StripeCardElement } from "@stripe/stripe-js";
+import { CardElement } from "./CardElement";
 
 interface CheckoutFormProps {
   disabled: boolean;
   taskId: string;
 }
-
-const CardElementContainer = styled.div`
-  margin-top: 12px;
-
-  .cardInput__container {
-    margin-bottom: 12px;
-    padding: 12px;
-    border: 1px solid #e6e6e6;
-    border-radius: 5px;
-  }
-
-  label {
-    margin-bottom: 0.25rem;
-    font-size: 0.93rem;
-    font-weight: 400;
-    font: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu,
-      Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-    color: rgb(48, 49, 61);
-  }
-`;
 
 export const CheckoutForm = ({ disabled, taskId }: CheckoutFormProps) => {
   const stripe = useStripe();
@@ -54,7 +34,7 @@ export const CheckoutForm = ({ disabled, taskId }: CheckoutFormProps) => {
 
     const result = await stripe.confirmCardPayment(clientSecret as string, {
       payment_method: {
-        card: elements.getElement(CardElement) as StripeCardElement,
+        card: elements.getElement(CardElementPrimitive) as StripeCardElement,
       },
     });
 
@@ -90,14 +70,8 @@ export const CheckoutForm = ({ disabled, taskId }: CheckoutFormProps) => {
         <AddressElement
           options={{ mode: "billing", allowedCountries: ["AU"] }}
         />
-
-        <CardElementContainer>
-          <label>Card details</label>
-          <div className="cardInput__container">
-            <CardElement id="card-element" options={{ hidePostalCode: true }} />
-          </div>
-        </CardElementContainer>
-        <ButtonStyles type="submit" primary>
+        <CardElement />
+        <ButtonStyles type="submit" primary disabled={disabled}>
           Checkout
         </ButtonStyles>
       </fieldset>
