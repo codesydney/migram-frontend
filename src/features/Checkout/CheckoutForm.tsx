cc69354e356dcbdf7acebdae1d9492e4a946015e
davidtaing <adavidtaing@gmail.com>
2022-12-23 11:59:15 +1100

    feat: finalize payment via onSubmit
    wip

--- src/features/Checkout/CheckoutForm.tsx
+++ src/features/Checkout/CheckoutForm.tsx
@@ -1,9 +1,15 @@
 "use client";
 import ButtonStyles from "../../components/styles/ButtonStyles";
-import { AddressElement, CardElement } from "@stripe/react-stripe-js";
+import {
+  AddressElement,
+  CardElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
 import styled from "styled-components";
-import React from "react";
+import React, { useState } from "react";
 import { useCreatePaymentIntent } from "./hooks";
+import { StripeCardElement } from "@stripe/stripe-js";
 
 interface CheckoutFormProps {
   disabled: boolean;
@@ -31,10 +37,50 @@ const CardElementContainer = styled.div`
 `;
 
 export const CheckoutForm = ({ disabled, taskId }: CheckoutFormProps) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [message, setMessage] = useState<string | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
   const { clientSecret } = useCreatePaymentIntent(taskId);
 
-  const onSubmit = (e: React.FormEvent) => {
+  const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsLoading(true);
+
+    const result = await stripe.confirmCardPayment(clientSecret as string, {
+      payment_method: {
+        card: elements.getElement(CardElement) as StripeCardElement,
+      },
+    });
+
+    if (result.error) {
+      console.log(result.error.message);
+
+      if (
+        result.error?.type === "card_error" ||
+        result.error?.type === "validation_error"
+      ) {
+        setMessage(result.error?.message);
+      } else {
+        setMessage("An unexpected error occurred.");
+      }
+    } else {
+      if (result.paymentIntent.status === "succeeded") {
+        // Show a success message to your customer
+        // There's a risk of the customer closing the window before callback
+        // execution. Set up a webhook or plugin to listen for the
+        // payment_intent.succeeded event that handles any business critical
+        // post-payment actions.
+        alert("Finalized Payment");
+      }
+    }
+
+    setIsLoading(false);
   };
 
   return (
