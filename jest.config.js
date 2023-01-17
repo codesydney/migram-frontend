8dbb11da0ded229f020696e3a0fa65652516af45
davidtaing <adavidtaing@gmail.com>
2023-01-17 19:17:14 +1100

    fix: path alias not being passed to jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 // jest.config.js
 const nextJest = require('next/jest')
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -13,6 +15,8 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/src/test/setup-files-after-env.ts'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  modulePaths: [compilerOptions.baseUrl], // <-- This will be set to 'baseUrl' value
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths /*, { prefix: '<rootDir>/' } */),
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
