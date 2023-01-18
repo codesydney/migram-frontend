a199ea5d4df2f4ab00abf426bc00c468ca4d3197
davidtaing <adavidtaing@gmail.com>
2023-01-18 18:05:17 +1100

    refactor(SignUpPage): connect to useSignUpForm

--- src/domains/users/Auth/features/SignUpPage/components/SignUpPage.tsx
+++ src/domains/users/Auth/features/SignUpPage/components/SignUpPage.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import Link from "next/link";
-import z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
   Button,
@@ -13,44 +10,7 @@ import {
   Text,
 } from "@shopify/polaris";
 import { TextField } from "@ComponentsV2/components/TextField";
-import { createUser, loginAndRedirect } from "@Users/Auth/api/AuthService";
-import { createCustomer } from "@Billing/Customers/api";
-
-const formSchema = z
-  .object({
-    name: z.string({ required_error: "Name is required" }),
-    email: z
-      .string({ required_error: "Email is required" })
-      .email({ message: "Invalid email address" }),
-    password: z
-      .string({ required_error: "Password is required" })
-      .min(8, "Password must be 8 or more characters"),
-    passwordConfirm: z.string().min(1, "Please enter a password confirmation"),
-  })
-  .refine((data) => data.password === data.passwordConfirm, {
-    message: "Passwords don't match",
-    path: ["passwordConfirm"],
-  });
-
-export type SignUpFormState = z.infer<typeof formSchema>;
-
-export const submitHandler = async (data: SignUpFormState) => {
-  await createUser(data);
-
-  const { email, name, password } = data;
-  await createCustomer({ email, name });
-
-  await loginAndRedirect({ email, password });
-};
-
-export const useSignUpForm = () => {
-  const { control, handleSubmit } = useForm<SignUpFormState>({
-    mode: "onBlur",
-    resolver: zodResolver(formSchema),
-  });
-
-  return { control, onSubmit: handleSubmit(submitHandler) };
-};
+import { useSignUpForm, SignUpFormState } from "../hooks";
 
 export const SignUpPage = () => {
   const { control, onSubmit } = useSignUpForm();
