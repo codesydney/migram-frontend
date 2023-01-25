2a9351d07bad5aa9fccbd5c8955e60b16f7064f1
davidtaing <adavidtaing@gmail.com>
2023-01-26 00:11:56 +1100

    refactor: remove old CreateTask page

--- src/pages/tasks/createtask.tsx
+++
@@ -1,8 +0,0 @@
-import CreateTask from "@Tasks/v1/CreateTask/CreateTask";
-import type { NextPage } from "next";
-
-const CreateTaskPage: NextPage = () => {
-  return <CreateTask />;
-};
-
-export default CreateTaskPage;
