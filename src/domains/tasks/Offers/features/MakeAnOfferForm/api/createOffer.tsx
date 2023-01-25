6b370a2e12ef67885e8c8fbe2a779bfd3742d76e
davidtaing <adavidtaing@gmail.com>
2023-01-25 22:29:13 +1100

    feat: add createOffer service function

---
+++ src/domains/tasks/Offers/features/MakeAnOfferForm/api/createOffer.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { MakeAnOfferFormState } from "../hooks";
+
+export const createOffer = async (
+  taskId: string,
+  { offerAmt, comments }: MakeAnOfferFormState
+) => {
+  return axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}api/v1/tasks/${taskId}/offers`,
+    {
+      offerAmt,
+      comments,
+    }
+  );
+};
