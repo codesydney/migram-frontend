fb915467a5e74a32b40d246c43055219efcbd9ba
davidtaing <adavidtaing@gmail.com>
2023-01-26 18:20:37 +1100

    feat: rework CreateTaskFormSchema
    - Add error messages to schema
    - Add dueDate coercion as ISODates are not parsed correctly by default
    - Made location field optional.

--- src/domains/tasks/CreateTask/types.tsx
+++ src/domains/tasks/CreateTask/types.tsx
@@ -34,21 +34,33 @@ export const TaskTimeEstimateSchema = z.enum([
 
 export const CreateTaskFormSchema = z.object({
   category: TaskCategorySchema.default("Cleaning"),
-  title: z.string().min(10),
-  details: z.string().min(25),
-  budget: z.number().gt(5),
+  title: z
+    .string({
+      required_error: "Title is required",
+    })
+    .min(10, { message: "Title must be at least 10 characters long" }),
+  details: z
+    .string({ required_error: "Details are required" })
+    .min(25, { message: "Details must be at least 25 characters long" }),
+  budget: z.coerce
+    .number({
+      invalid_type_error: "Budget must be a number",
+    })
+    .gt(5, { message: "Budget must be at least $5" }),
   timeOfArrival: TaskTimeOfArrivalSchema.default("7am-10am"),
   timeEstimate: TaskTimeEstimateSchema.default("1-3hrs"),
   status: TaskStatusSchema.default("open"),
-  dueDate: z
-    .date()
+  dueDate: z.coerce
+    .date({ invalid_type_error: "Due date must be a date" })
     .min(new Date(), { message: "Due date must be in the future" }),
   photos: z.optional(z.array(z.string())),
-  location: z.object({
-    name: z.string(),
-    type: z.string(),
-    coordinates: z.tuple([z.number(), z.number()]),
-  }),
+  location: z
+    .object({
+      name: z.string(),
+      type: z.string(),
+      coordinates: z.tuple([z.number(), z.number()]),
+    })
+    .optional(),
 });
 
 export type CreateTaskFormState = z.infer<typeof CreateTaskFormSchema>;
