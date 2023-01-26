eb375f1c24a3d9c5c8a084d4f8af8fa53b2a5a85
davidtaing <adavidtaing@gmail.com>
2023-01-26 23:41:34 +1100

    feat: change AddressSchema.state to enum

--- src/types/schemas.ts
+++ src/types/schemas.ts
@@ -1,14 +1,25 @@
 import { z } from "zod";
 
-export const addressSchema = z.object({
+export const StateSchema = z.enum([
+  "NSW",
+  "ACT",
+  "NT",
+  "QLD",
+  "SA",
+  "TAS",
+  "VIC",
+  "WA",
+]);
+
+export const AddressSchema = z.object({
   line1: z.string().min(1, "Please enter address line 1"),
   line2: z.string().optional(),
   city: z.string().min(1, "Please enter a city"),
-  state: z.string().min(1, "Please enter a state"),
+  state: StateSchema,
   postal_code: z
     .string()
     .min(1, "Please enter a postcode")
     .length(4, "Postcode must be 4 digits"),
 });
 
-export type AddressSchema = z.infer<typeof addressSchema>;
+export type Address = z.infer<typeof AddressSchema>;
