d63283c3223ac9c69888f6dd5bb46efae53bf038
davidtaing <adavidtaing@gmail.com>
2023-01-26 23:42:13 +1100

    feat: CreateTask api request
    - add happy path implementation of the POST /task request

--- src/domains/tasks/CreateTask/hooks/useCreateTaskForm.tsx
+++ src/domains/tasks/CreateTask/hooks/useCreateTaskForm.tsx
@@ -1,8 +1,17 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { CreateTaskFormSchema, CreateTaskFormState } from "../types";
+import { createTask } from "../api";
+import { routerPush } from "@Utils/router";
 
-export const submitHandler = async (data: CreateTaskFormState) => {};
+export const submitHandler = async (data: CreateTaskFormState) => {
+  try {
+    const response = await createTask(data);
+    routerPush("/tasks");
+  } catch (error: any) {
+    console.error(error);
+  }
+};
 
 export const useCreateTaskForm = () => {
   const { control, handleSubmit } = useForm<CreateTaskFormState>({
