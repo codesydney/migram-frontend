3d36287396e717a1291f8d1f8776f9c88059db8d
davidtaing <adavidtaing@gmail.com>
2023-02-07 23:58:38 +1100

    feat(TaskCard): convert top-level div to article

--- src/domains/tasks/ViewTasks/components/TasksPage.tsx
+++ src/domains/tasks/ViewTasks/components/TasksPage.tsx
@@ -2,13 +2,13 @@ import { Card } from "@shopify/polaris";
 
 export const TaskCard = () => {
   return (
-    <div aria-label="Task Card">
+    <article aria-label="Task Card">
       <Card sectioned>
         <Card.Header title={"Title"}></Card.Header>
         <Card.Section title="Details"></Card.Section>
         <Card.Section title="Offers"></Card.Section>
       </Card>
-    </div>
+    </article>
   );
 };
 
