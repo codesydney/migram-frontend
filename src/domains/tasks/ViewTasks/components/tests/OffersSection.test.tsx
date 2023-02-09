78ca0bf4f846c3dd1638f7b45101b57fcd5459c2
davidtaing <adavidtaing@gmail.com>
2023-02-09 23:04:29 +1100

    tests: update component props to fix failed tests

--- src/domains/tasks/ViewTasks/components/tests/OffersSection.test.tsx
+++ src/domains/tasks/ViewTasks/components/tests/OffersSection.test.tsx
@@ -4,9 +4,31 @@ import userEvent from "@testing-library/user-event";
 import { OffersSection } from "../TasksPage";
 
 import { renderWithPolarisTestProvider } from "src/test/utils";
+import { Task } from "@Tasks/common/types";
+
+const task: Task = {
+  location: {
+    line1: "123 Fake St",
+    city: "Sydney",
+    state: "NSW",
+    postal_code: "2000",
+  },
+  status: "open",
+  photos: [],
+  category: "Cleaning",
+  title: "End of Lease Cleaning",
+  details:
+    "Hi, I'm moving out of my 2 bedroom unit in Sydney. We'll need it cleaned before handing off the keys to the agent.",
+  budget: 200,
+  timeOfArrival: "7am-10am",
+  timeEstimate: "1-3hrs",
+  dueDate: new Date("2023-01-27T00:00:00.000Z"),
+  customerId: "cus_Me9Lz7memmLIEB",
+  id: "63d26e6651167241c5f238a4",
+};
 
 function renderOffersSection() {
-  return renderWithPolarisTestProvider(<OffersSection />);
+  return renderWithPolarisTestProvider(<OffersSection task={task} />);
 }
 
 test("Smoke test if it renders", () => {
--- src/domains/tasks/ViewTasks/components/tests/TasksPage.test.tsx
+++ src/domains/tasks/ViewTasks/components/tests/TasksPage.test.tsx
@@ -5,11 +5,11 @@ import { TasksPage } from "../TasksPage";
 import { renderWithPolarisTestProvider } from "src/test/utils";
 
 test("Smoke Test if it renders", () => {
-  renderWithPolarisTestProvider(<TasksPage />);
+  renderWithPolarisTestProvider(<TasksPage status="authenticated" />);
   expect(screen.getByLabelText("Customer Tasks Page")).toBeTruthy();
 });
 
 test("It renders TaskCard as a child", () => {
-  renderWithPolarisTestProvider(<TasksPage />);
+  renderWithPolarisTestProvider(<TasksPage status="authenticated" />);
   expect(screen.getByLabelText("Task Card")).toBeTruthy();
 });
