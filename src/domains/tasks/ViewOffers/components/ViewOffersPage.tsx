467d0ce58c73f536b3adcd6a00bef8bb552ff2a5
davidtaing <adavidtaing@gmail.com>
2023-02-10 17:21:28 +1100

    feat: add temporary /offersv2 page

--- src/domains/tasks/ViewOffers/components/ViewOffersPage.tsx
+++ src/domains/tasks/ViewOffers/components/ViewOffersPage.tsx
@@ -1,3 +1,8 @@
-export function ViewOffersPage() {
+export function ViewOffersPage({
+  status,
+}: {
+  status: "authenticated" | "loading" | "unauthenticated";
+}) {
+  console.log(status);
   return <div aria-label="View Offers Page"></div>;
 }
