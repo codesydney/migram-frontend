cd1ced485668ad1ea0b4f63c49e00628d445cc39
davidtaing <adavidtaing@gmail.com>
2023-02-10 21:14:51 +1100

    feat(ViewOffersPage): remove feature flag

--- src/pages/offers.tsx
+++ src/pages/offers.tsx
@@ -1,11 +1,8 @@
 import { useSession } from "next-auth/react";
 
 import { ViewOffersPage } from "@Tasks/ViewOffers/components/ViewOffersPage";
-import { FeatureFlag } from "@Components/utils/FeatureFlag";
 
-function OffersPage() {
+export default function OffersPage() {
   const { status } = useSession();
   return <ViewOffersPage status={status} />;
 }
-
-export default FeatureFlag(OffersPage, { isPage: true });
