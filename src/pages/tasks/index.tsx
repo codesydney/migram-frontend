e9440388f01df06c86cd041c0c3ab36cfc36ca16
David Taing <adavidtaing@gmail.com>
2023-02-10 16:43:54 +1100

    feat(TasksPage): remove FeatureFlag

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,13 +1,8 @@
-import { FeatureFlag } from "@Components/utils/FeatureFlag";
 import { TasksPage as TaskPagePrimitive } from "@Tasks/ViewTasks";
 import { useSession } from "next-auth/react";
 
-const TasksPage = () => {
+export default function TasksPage() {
   const { status } = useSession();
 
   return <TaskPagePrimitive status={status} />;
 };
-
-export default FeatureFlag(TasksPage, {
-  isPage: true,
-});
