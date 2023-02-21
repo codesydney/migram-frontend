cf245fe8f4552f40b58f27a34cd3e253c782673d
davidtaing <adavidtaing@gmail.com>
2023-02-22 00:24:01 +1100

    feat: add minimal PageWithNotifications
    - add new component with minimal implementation
    
    - add smoke test if it renders

---
+++ src/components/PageWithNotifcations.tsx
@@ -0,0 +1,9 @@
+import { Page, Layout } from "@shopify/polaris";
+
+export function PageWithNotifications() {
+  return (
+    <Page title="Create Task">
+      <Layout></Layout>
+    </Page>
+  );
+}
---
+++ src/components/tests/PageWithNotifications.test.tsx
@@ -0,0 +1,14 @@
+import { NotificationProps } from "../Notification";
+import { renderWithPolarisTestProvider } from "src/test/utils";
+import { PageWithNotifications } from "../PageWithNotifcations";
+
+function setupRender(componentProps?: NotificationProps) {
+  return renderWithPolarisTestProvider(
+    <PageWithNotifications {...componentProps} />
+  );
+}
+
+test("Smoke test if BaseNotification renders", () => {
+  const { baseElement } = setupRender();
+  expect(baseElement).toBeInTheDocument();
+});
