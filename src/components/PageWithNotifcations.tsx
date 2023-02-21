cb59b6bea82ba11a84650de4293345e4bfc5e3e9
davidtaing <adavidtaing@gmail.com>
2023-02-21 23:15:47 +1100

    feat: define props for PageWIthNotifications

--- src/components/PageWithNotifcations.tsx
+++ src/components/PageWithNotifcations.tsx
@@ -1,9 +1,23 @@
 import { Page, Layout } from "@shopify/polaris";
+import { ComponentPropsWithoutRef } from "react";
 
-export function PageWithNotifications() {
+export type PageWithNotificationsProps = ComponentPropsWithoutRef<
+  typeof Page
+> & {
+  notification?: React.ReactNode;
+};
+
+export function PageWithNotifications({
+  children,
+  notification,
+  ...otherProps
+}: PageWithNotificationsProps) {
   return (
-    <Page title="Create Task">
-      <Layout></Layout>
+    <Page {...otherProps}>
+      <Layout>
+        {notification}
+        {children}
+      </Layout>
     </Page>
   );
 }
