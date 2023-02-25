e2742244d04cae8d84ed11e34f8e05be4ab4f2a7
davidtaing <adavidtaing@gmail.com>
2023-02-26 00:25:19 +1100

    feat: convert Map to ImmutableMap
    for ApiEventsProvider value

---
+++ src/common/ApiResponse/ApiEventsContext.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren, createContext, useContext } from "react";
+import { ImmutableMap } from "map-immute";
+
+import { ApiEvent } from "./types";
+
+export type ApiEventsMap = ImmutableMap<string, ApiEvent>;
+
+const ApiEventsContext = createContext<ApiEventsMap | undefined>(undefined);
+
+export function ApiEventsProvider({ children }: PropsWithChildren<{}>) {
+  return (
+    <ApiEventsContext.Provider value={new ImmutableMap<string, ApiEvent>()}>
+      {children}
+    </ApiEventsContext.Provider>
+  );
+}
+
+export function useApiEvents() {
+  const context = useContext(ApiEventsContext);
+
+  if (!context) {
+    throw new Error("useApiEvents must be used within ApiEventsProvider");
+  }
+
+  return context;
+}
