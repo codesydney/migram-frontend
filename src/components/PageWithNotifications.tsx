78635540842be516e420542411625c98cdc27900
davidtaing <adavidtaing@gmail.com>
2023-03-02 18:15:32 +1100

    feat: add onDismiss to BaseNotifications
    This allows the user to close the Notification

--- src/components/PageWithNotifications.tsx
+++ src/components/PageWithNotifications.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Page, Layout } from "@shopify/polaris";
 import { ComponentPropsWithoutRef } from "react";
 import { useApiEvents } from "src/common/ApiResponse/ApiEventsContext";
@@ -6,11 +5,17 @@ import { BaseNotification } from "./Notification";
 
 export type PageWithNotificationsProps = ComponentPropsWithoutRef<typeof Page>;
 
+const EventLevelToNotificationStatus = {
+  info: "info",
+  warn: "warning",
+  error: "critical",
+} as const;
+
 export function PageWithNotifications({
   children,
   ...otherProps
 }: PageWithNotificationsProps) {
-  const { apiEvents } = useApiEvents();
+  const { apiEvents, dispatchApiEvents } = useApiEvents();
   const events = [...apiEvents.values()];
 
   return (
@@ -20,7 +25,13 @@ export function PageWithNotifications({
           <BaseNotification
             key={event.id}
             title={event.title}
-            status="critical"
+            status={EventLevelToNotificationStatus[event.level]}
+            onDismiss={() => {
+              dispatchApiEvents({
+                type: "delete",
+                id: event.id,
+              });
+            }}
           />
         ))}
         {children}
