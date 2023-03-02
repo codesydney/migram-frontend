0fe50bd2e1383638c6aa243f67ce414e2046b16f
davidtaing <adavidtaing@gmail.com>
2023-03-02 20:11:21 +1100

    test: PageWIthNotifications
    Cases:
    - does not disaply a Notification when there are no API events
    - displays a Notification when there is an API event
    - notification disappears when closed

--- src/components/tests/PageWithNotifications.test.tsx
+++ src/components/tests/PageWithNotifications.test.tsx
@@ -1,15 +1,27 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
 import { renderWithPolarisTestProvider } from "src/test/utils";
 import {
   PageWithNotifications,
   PageWithNotificationsProps,
 } from "../PageWithNotifications";
-import { ApiEventsProvider } from "src/common/ApiResponse/ApiEventsContext";
+import {
+  ApiEventsProvider,
+  InitialApiEventsState,
+} from "src/common/ApiResponse/ApiEventsContext";
+import userEvent from "@testing-library/user-event";
 
-function setupRender(componentProps?: PageWithNotificationsProps) {
+type SetupRenderProps = {
+  componentProps?: PageWithNotificationsProps;
+  initialProviderState?: InitialApiEventsState;
+};
+
+function setupRender({
+  componentProps,
+  initialProviderState,
+}: SetupRenderProps = {}) {
   return renderWithPolarisTestProvider(
-    <ApiEventsProvider>
+    <ApiEventsProvider initialState={initialProviderState}>
       <PageWithNotifications {...componentProps} />
     </ApiEventsProvider>
   );
@@ -18,4 +30,67 @@ function setupRender(componentProps?: PageWithNotificationsProps) {
 test("Smoke test if BaseNotification renders", () => {
   const { baseElement } = setupRender();
   expect(baseElement).toBeInTheDocument();
+});
+
+it("does not display a notification when there are no API Events", () => {
+  setupRender();
+
+  const notification = screen.queryByText(/^test notification$/i);
+
+  expect(notification).toBeFalsy();
+});
+
+it("displays a notification when there is an API Event", () => {
+  const intialApiEventsState: InitialApiEventsState = [
+    [
+      "1",
+      {
+        id: "1",
+        isError: true,
+        title: "Test Notification",
+        status: 400,
+        statusText: "Bad Request",
+        level: "error",
+      },
+    ],
+  ] as const;
+
+  setupRender({ initialProviderState: intialApiEventsState });
+
+  const notification = screen.getByText(/^test notification$/i);
+
+  expect(notification).toBeTruthy();
+});
+
+it("notification disappears when closed", async () => {
+  const intialApiEventsState: InitialApiEventsState = [
+    [
+      "1",
+      {
+        id: "1",
+        isError: true,
+        title: "Test Notification",
+        status: 400,
+        statusText: "Bad Request",
+        level: "error",
+      },
+    ],
+  ] as const;
+
+  const user = userEvent.setup();
+  setupRender({ initialProviderState: intialApiEventsState });
+
+  let notification = screen.queryByText(/^test notification$/i);
+
+  expect(notification).toBeTruthy();
+
+  const dismissNotificationButton = screen.getByLabelText(
+    /^dismiss notification$/i
+  );
+
+  await user.click(dismissNotificationButton);
+
+  notification = screen.queryByText(/^test notification$/i);
+
+  expect(notification).toBeFalsy();
 });
