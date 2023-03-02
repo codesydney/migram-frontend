ac916b9abf674db06dc764a14dc19d1f354a8266
davidtaing <adavidtaing@gmail.com>
2023-03-02 14:08:24 +1100

    feat: add apiEventsReducer
    to simplify updating ApiEvents state

--- src/common/ApiResponse/ApiEventsContext.tsx
+++ src/common/ApiResponse/ApiEventsContext.tsx
@@ -1,50 +1,96 @@
 "use client";
 
-import { PropsWithChildren, createContext, useContext, useState } from "react";
+import {
+  Dispatch,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useReducer,
+} from "react";
 import { ImmutableMap } from "map-immute";
 
 import { ApiEvent } from "./types";
 
 export type ApiEventsMap = ImmutableMap<string, ApiEvent>;
+
+export type ApiEventAction =
+  | {
+      type: "set";
+      event: ApiEvent;
+    }
+  | {
+      type: "delete";
+      id: string;
+    }
+  | {
+      type: "clear";
+    };
+
+export function apiEventsReducer(
+  state: ApiEventsMap,
+  action: ApiEventAction
+): ApiEventsMap {
+  switch (action.type) {
+    case "set":
+      return state.set(action.event.id, action.event);
+    case "delete":
+      const result = state.safeDelete(action.id);
+      if (!result) return state;
+      return result;
+    case "clear":
+      return state.safeClear();
+    default:
+      return state;
+  }
+}
+
 export type ApiEventsContextValues = {
   apiEvents: ApiEventsMap;
-  setApiEvents: (apiEvents: ApiEventsMap) => void;
+  dispatchApiEvents: Dispatch<ApiEventAction>;
 };
 
 const ApiEventsContext = createContext<ApiEventsContextValues | undefined>(
   undefined
 );
 
-export function ApiEventsProvider({ children }: PropsWithChildren<{}>) {
-  const testEvents = [
-    [
-      "1",
-      {
-        id: "1",
-        isError: true,
-        title: "string",
-        status: 400,
-        statusText: "Bad Request",
-      },
-    ],
-    [
-      "2",
-      {
-        id: "2",
-        isError: true,
-        title: "Event Two",
-        status: 400,
-        statusText: "Bad Request",
-      },
-    ],
-  ] as const;
+const testEvents = [
+  [
+    "1",
+    {
+      id: "1",
+      isError: true,
+      title: "string",
+      status: 400,
+      statusText: "Bad Request",
+    },
+  ],
+  [
+    "2",
+    {
+      id: "2",
+      isError: true,
+      title: "Event Two",
+      status: 400,
+      statusText: "Bad Request",
+    },
+  ],
+] as const;
 
-  const [apiEvents, setApiEvents] = useState<ApiEventsMap>(
-    () => new ImmutableMap<string, ApiEvent>(testEvents)
+export type ApiEventsProviderProps = {
+  initialState?: ApiEventsMap;
+} & PropsWithChildren<{}>;
+
+export function ApiEventsProvider({
+  children,
+  initialState = new ImmutableMap<string, ApiEvent>(testEvents),
+}: ApiEventsProviderProps) {
+  const [apiEvents, dispatchApiEvents] = useReducer(
+    apiEventsReducer,
+    initialState
   );
 
   return (
-    <ApiEventsContext.Provider value={{ apiEvents, setApiEvents }}>
+    <ApiEventsContext.Provider value={{ apiEvents, dispatchApiEvents }}>
       {children}
     </ApiEventsContext.Provider>
   );
