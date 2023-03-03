6c63b63a91f8556accdadd956ec90c4925a98bc8
davidtaing <adavidtaing@gmail.com>
2023-03-03 22:06:13 +1100

    style: remove StyledDiv in prepation for rewrite

--- src/domains/billing/Checkout/CheckoutDetails.tsx
+++ src/domains/billing/Checkout/CheckoutDetails.tsx
@@ -1,46 +1,4 @@
 import { Task } from "@Tasks/common/types";
-import styled from "styled-components";
-
-const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-
-  .provider-container {
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-
-    > p {
-      margin: 0;
-    }
-  }
-
-  .temp-invoice-pdf {
-    border: 1px solid #d6d3d1;
-    height: 2rem;
-  }
-
-  .temp-provider-avatar {
-    border: 1px solid #d6d3d1;
-    border-radius: 100%;
-    width: 3rem;
-    height: 3rem;
-  }
-
-  .checkout-details-footer {
-    .price {
-      display: flex;
-      align-items: center;
-      margin-top: 1.5rem;
-      justify-content: space-between;
-    }
-
-    h5 {
-      margin: 0;
-    }
-  }
-`;
 
 interface CheckoutDetailsProps {
   task: Task;
@@ -51,7 +9,7 @@ export const CheckoutDetails = ({ task, isLoading }: CheckoutDetailsProps) => {
   if (isLoading) return <div>Loading</div>;
 
   return (
-    <StyledDiv>
+    <div>
       <div className="checkout-details-body">
         <h2>Task Details</h2>
         <h4>{task.details}</h4>
@@ -76,6 +34,6 @@ export const CheckoutDetails = ({ task, isLoading }: CheckoutDetailsProps) => {
           </h5>
         </div>
       </div>
-    </StyledDiv>
+    </div>
   );
 };
