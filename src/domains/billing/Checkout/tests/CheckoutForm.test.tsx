030013213de35a50874c11e1fafa547299774e11
davidtaing <adavidtaing@gmail.com>
2023-03-03 23:04:39 +1100

    fix: failing test due to missing Provider
    ApiEventsProvider was not rendered with the CheckoutForm tests.

--- src/domains/billing/Checkout/tests/CheckoutForm.test.tsx
+++ src/domains/billing/Checkout/tests/CheckoutForm.test.tsx
@@ -2,12 +2,15 @@ import { render, screen } from "@testing-library/react";
 import { CheckoutForm } from "../CheckoutForm";
 import { ElementsWrapper } from "src/components/ElementsWrapper";
 import { renderWithPolarisTestProvider } from "src/test/utils";
+import { ApiEventsProvider } from "src/common/ApiResponse/ApiEventsContext";
 
 describe("CheckoutForm", () => {
   test("Smoke test if it renders", () => {
     renderWithPolarisTestProvider(
       <ElementsWrapper>
-        <CheckoutForm isPageLoading={false} taskId="0" />
+        <ApiEventsProvider>
+          <CheckoutForm isPageLoading={false} taskId="0" />
+        </ApiEventsProvider>
       </ElementsWrapper>
     );
 
