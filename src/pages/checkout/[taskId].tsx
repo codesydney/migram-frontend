4f509b6dc257d5639f4c2b16891f7219d443a464
davidtaing <adavidtaing@gmail.com>
2023-03-04 20:37:17 +1100

    fix: CheckoutPage fetching undefined task
    taskId is undefined upon CheckoutRoute's inital render.
    
    This commit adds a change to conditionally render the
    CheckoutPage when the taskId is ready.

--- src/pages/checkout/[taskId].tsx
+++ src/pages/checkout/[taskId].tsx
@@ -7,7 +7,11 @@ import { FeatureFlag } from "src/components/utils/FeatureFlag";
 
 const CheckoutRoute = () => {
   const router = useRouter();
-  const { taskId } = router.query as { taskId: string };
+  const { taskId } = router.query as { taskId: string | undefined };
+
+  if (taskId === undefined) {
+    return <></>;
+  }
 
   return <CheckoutPage taskId={taskId} />;
 };
