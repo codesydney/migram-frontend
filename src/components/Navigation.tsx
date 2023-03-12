239edfa1fee33f730107ac69ab2487045d425323
davidtaing <adavidtaing@gmail.com>
2023-03-12 17:11:41 +1100

    feat(Navigation): show menu items
    depending on user type
    
    - display Tasks for Customers
    - display Offers for Providers

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,26 +1,40 @@
 import { Navigation as NavigationPrimitive } from "@shopify/polaris";
 import { FinancesMajor, HomeMajor, JobsMajor } from "@shopify/polaris-icons";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { Session } from "next-auth";
+
+function getMenuItems(session: Session | null) {
+  if (session?.user.customerId) {
+    return [
+      {
+        url: "/tasks",
+        label: "Tasks",
+        icon: JobsMajor,
+      },
+    ];
+  }
+
+  if (session?.user.providerId) {
+    return [
+      {
+        url: "/offers",
+        label: "Offers",
+        icon: FinancesMajor,
+      },
+    ];
+  }
+
+  return [];
+}
 
 export const Navigation = () => {
   const router = useRouter();
+  const { data: session } = useSession();
 
   return (
     <NavigationPrimitive location={router.asPath}>
-      <NavigationPrimitive.Section
-        items={[
-          {
-            url: "/tasks",
-            label: "Tasks",
-            icon: JobsMajor,
-          },
-          {
-            url: "/offers",
-            label: "Offers",
-            icon: FinancesMajor,
-          },
-        ]}
-      />
+      <NavigationPrimitive.Section items={getMenuItems(session)} />
     </NavigationPrimitive>
   );
 };
