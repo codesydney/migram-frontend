5be6b2fc02e77177b2665fd089169167c26d68b2
davidtaing <adavidtaing@gmail.com>
2023-03-17 18:48:45 +1100

    fix: form styling
    This commit adds styling that fixes the follow forms from overflowing
    on mobile viewports:
    - Login
    - Customer Signup Page
    - Provider Signup Page
    - Create Task Page

--- src/domains/tasks/CreateTask/components/CreateTaskPage.tsx
+++ src/domains/tasks/CreateTask/components/CreateTaskPage.tsx
@@ -1,4 +1,4 @@
-import { Form, FormLayout, Button, Text } from "@shopify/polaris";
+import { Form, FormLayout, Button, Text, Layout } from "@shopify/polaris";
 
 import { TextField, Select } from "src/components/index";
 
@@ -10,9 +10,8 @@ import {
   TaskTimeEstimateSchema,
   TaskTimeOfArrivalSchema,
 } from "@Tasks/common/types";
-import {
-  PageWithNotifications,
-} from "src/common/features/notifications";
+import { PageWithNotifications } from "src/common/features/notifications";
+import { FormPaddingDiv } from "src/components/FormPaddingDiv";
 
 export const CreateTaskPage = () => {
   const { control, onSubmit } = useCreateTaskForm();
@@ -20,110 +19,114 @@ export const CreateTaskPage = () => {
   return (
     <div aria-label="Create Task Page">
       <PageWithNotifications title="Create Task">
-        <Form onSubmit={onSubmit}>
-          <FormLayout>
-            <TextField<CreateTaskFormState>
-              name="title"
-              label="Title"
-              type="text"
-              requiredIndicator
-              autoComplete="off"
-              control={control}
-            />
+        <Layout.Section>
+          <FormPaddingDiv>
+            <Form onSubmit={onSubmit}>
+              <FormLayout>
+                <TextField<CreateTaskFormState>
+                  name="title"
+                  label="Title"
+                  type="text"
+                  requiredIndicator
+                  autoComplete="off"
+                  control={control}
+                />
 
-            <TextField<CreateTaskFormState>
-              name="budget"
-              label="Budget"
-              type="currency"
-              requiredIndicator
-              autoComplete="off"
-              control={control}
-            />
+                <TextField<CreateTaskFormState>
+                  name="budget"
+                  label="Budget"
+                  type="currency"
+                  requiredIndicator
+                  autoComplete="off"
+                  control={control}
+                />
 
-            <Select<CreateTaskFormState>
-              options={TaskCategorySchema.options}
-              label="Category"
-              requiredIndicator
-              name="category"
-              control={control}
-            />
+                <Select<CreateTaskFormState>
+                  options={TaskCategorySchema.options}
+                  label="Category"
+                  requiredIndicator
+                  name="category"
+                  control={control}
+                />
 
-            <TextField<CreateTaskFormState>
-              name="details"
-              label="Details"
-              requiredIndicator
-              autoComplete="off"
-              control={control}
-            />
+                <TextField<CreateTaskFormState>
+                  name="details"
+                  label="Details"
+                  requiredIndicator
+                  autoComplete="off"
+                  control={control}
+                />
 
-            <TextField<CreateTaskFormState>
-              name="dueDate"
-              label="Due Date"
-              type="date"
-              requiredIndicator
-              autoComplete="off"
-              control={control}
-            />
+                <TextField<CreateTaskFormState>
+                  name="dueDate"
+                  label="Due Date"
+                  type="date"
+                  requiredIndicator
+                  autoComplete="off"
+                  control={control}
+                />
 
-            <Select<CreateTaskFormState>
-              name="timeOfArrival"
-              label="Time of Arrival"
-              requiredIndicator
-              options={TaskTimeOfArrivalSchema.options}
-              control={control}
-            />
-            <Select<CreateTaskFormState>
-              name="timeEstimate"
-              label="Estimated Time"
-              requiredIndicator
-              options={TaskTimeEstimateSchema.options}
-              control={control}
-            />
+                <Select<CreateTaskFormState>
+                  name="timeOfArrival"
+                  label="Time of Arrival"
+                  requiredIndicator
+                  options={TaskTimeOfArrivalSchema.options}
+                  control={control}
+                />
+                <Select<CreateTaskFormState>
+                  name="timeEstimate"
+                  label="Estimated Time"
+                  requiredIndicator
+                  options={TaskTimeEstimateSchema.options}
+                  control={control}
+                />
 
-            <Text as="h3" variant="headingMd">
-              Address:
-            </Text>
-            <TextField<CreateTaskFormState>
-              name="location.line1"
-              label="Line 1"
-              requiredIndicator
-              autoComplete="off"
-              control={control}
-            />
-            <TextField<CreateTaskFormState>
-              name="location.line2"
-              label="Line 2"
-              autoComplete="off"
-              control={control}
-            />
-            <FormLayout.Group condensed>
-              <TextField<CreateTaskFormState>
-                name="location.city"
-                label="City"
-                requiredIndicator
-                autoComplete="off"
-                control={control}
-              />
-              <Select<CreateTaskFormState>
-                name="location.state"
-                label="State"
-                requiredIndicator
-                options={StateSchema.options}
-                control={control}
-              />
-              <TextField<CreateTaskFormState>
-                name="location.postal_code"
-                label="Postcode"
-                requiredIndicator
-                autoComplete="off"
-                control={control}
-              />
-            </FormLayout.Group>
-            <Button primary submit size="large">
-              Submit
-            </Button>
-          </FormLayout>
-        </Form>
+                <Text as="h3" variant="headingMd">
+                  Address:
+                </Text>
+                <TextField<CreateTaskFormState>
+                  name="location.line1"
+                  label="Line 1"
+                  requiredIndicator
+                  autoComplete="off"
+                  control={control}
+                />
+                <TextField<CreateTaskFormState>
+                  name="location.line2"
+                  label="Line 2"
+                  autoComplete="off"
+                  control={control}
+                />
+                <FormLayout.Group condensed>
+                  <TextField<CreateTaskFormState>
+                    name="location.city"
+                    label="City"
+                    requiredIndicator
+                    autoComplete="off"
+                    control={control}
+                  />
+                  <Select<CreateTaskFormState>
+                    name="location.state"
+                    label="State"
+                    requiredIndicator
+                    options={StateSchema.options}
+                    control={control}
+                  />
+                  <TextField<CreateTaskFormState>
+                    name="location.postal_code"
+                    label="Postcode"
+                    requiredIndicator
+                    autoComplete="off"
+                    control={control}
+                  />
+                </FormLayout.Group>
+                <Button primary submit size="large">
+                  Submit
+                </Button>
+              </FormLayout>
+            </Form>
+          </FormPaddingDiv>
+        </Layout.Section>
       </PageWithNotifications>
     </div>
   );
--- src/domains/users/CustomerSignUp/components/SignUpPage.tsx
+++ src/domains/users/CustomerSignUp/components/SignUpPage.tsx
@@ -6,13 +6,13 @@ import {
   Checkbox,
   Form,
   FormLayout,
+  Layout,
   Text,
 } from "@shopify/polaris";
-import {
-  PageWithNotifications,
-} from "src/common/features/notifications";
+import { PageWithNotifications } from "src/common/features/notifications";
 import { TextField } from "src/components/TextField";
 import { useSignUpForm, SignUpFormState } from "../hooks";
+import { FormPaddingDiv } from "src/components/FormPaddingDiv";
 
 export const SignUpPage = () => {
   const { control, onSubmit } = useSignUpForm();
@@ -20,58 +20,62 @@ export const SignUpPage = () => {
 
   return (
     <PageWithNotifications title="Sign Up">
-      <Form onSubmit={onSubmit}>
-        <FormLayout>
-          <TextField<SignUpFormState>
-            name="name"
-            label="Name"
-            autoComplete="name"
-            control={control}
-          />
-          <TextField<SignUpFormState>
-            name="email"
-            label="Email"
-            type="email"
-            autoComplete="email"
-            control={control}
-          />
-          <TextField<SignUpFormState>
-            id="password"
-            name="password"
-            label="Password"
-            type={showPassword ? "text" : "password"}
-            autoComplete="new-password"
-            control={control}
-          />
-          <TextField<SignUpFormState>
-            id="passwordConfirm"
-            name="passwordConfirm"
-            label="Confirm Password"
-            type={showPassword ? "text" : "password"}
-            autoComplete="new-password"
-            control={control}
-          />
-          <Checkbox
-            label="Show Password"
-            checked={showPassword}
-            onChange={() => setShowPassword(!showPassword)}
-          />
-          <Button primary submit size="large">
-            Submit
-          </Button>
+      <Layout.Section>
+        <FormPaddingDiv>
+          <Form onSubmit={onSubmit}>
+            <FormLayout>
+              <TextField<SignUpFormState>
+                name="name"
+                label="Name"
+                autoComplete="name"
+                control={control}
+              />
+              <TextField<SignUpFormState>
+                name="email"
+                label="Email"
+                type="email"
+                autoComplete="email"
+                control={control}
+              />
+              <TextField<SignUpFormState>
+                id="password"
+                name="password"
+                label="Password"
+                type={showPassword ? "text" : "password"}
+                autoComplete="new-password"
+                control={control}
+              />
+              <TextField<SignUpFormState>
+                id="passwordConfirm"
+                name="passwordConfirm"
+                label="Confirm Password"
+                type={showPassword ? "text" : "password"}
+                autoComplete="new-password"
+                control={control}
+              />
+              <Checkbox
+                label="Show Password"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+              />
+              <Button primary submit size="large">
+                Submit
+              </Button>
 
-          <Text as="h2" variant="headingSm">
-            Want to earn money on Tasks instead?{" "}
-            <Link
-              href="/providers/signup"
-              className="Polaris-Link"
-              data-polaris-unstyled="true"
-            >
-              Sign Up as Provider.
-            </Link>
-          </Text>
-        </FormLayout>
-      </Form>
+              <Text as="h2" variant="headingSm">
+                Want to earn money on Tasks instead?{" "}
+                <Link
+                  href="/providers/signup"
+                  className="Polaris-Link"
+                  data-polaris-unstyled="true"
+                >
+                  Sign Up as Provider.
+                </Link>
+              </Text>
+            </FormLayout>
+          </Form>
+        </FormPaddingDiv>
+      </Layout.Section>
     </PageWithNotifications>
   );
 };
