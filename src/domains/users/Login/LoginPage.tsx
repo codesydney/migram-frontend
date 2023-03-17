fe387cd438a949814ee874802c74ed78856d1097
davidtaing <adavidtaing@gmail.com>
2023-03-17 20:16:14 +1100

    feat: add success notification to Login Page

--- src/domains/users/Login/LoginPage.tsx
+++ src/domains/users/Login/LoginPage.tsx
@@ -12,11 +12,16 @@ import {
   Layout,
   Text,
 } from "@shopify/polaris";
-import { PageWithNotifications } from "src/common/features/notifications";
+import {
+  NotificationsAction,
+  PageWithNotifications,
+  useNotifications,
+} from "src/common/features/notifications";
 import { routerPush } from "@Utils/router";
 
 import { TextField } from "src/components/TextField";
 import { signIn } from "../common/api";
+import { createNotification } from "src/common/features/notifications/utils";
 import { FormPaddingDiv } from "src/components/FormPaddingDiv";
 
 const formSchema = z.object({
@@ -31,13 +36,29 @@ export type LoginFormState = z.infer<typeof formSchema>;
 
 export const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const { dispatchNotifications } = useNotifications();
   const { control, handleSubmit } = useForm<LoginFormState>({
     mode: "onBlur",
     resolver: zodResolver(formSchema),
   });
 
   const handleLogin = async (formValues: LoginFormState) => {
-    const data = await signIn(formValues);
+    const data = await signIn(formValues).then((data) => {
+      const action: NotificationsAction = {
+        type: "set",
+        event: createNotification({
+          isError: false,
+          title: "Successfully logged in.",
+          type: "toast",
+          status: "success",
+          source: "Login Success",
+        }),
+      };
+
+      dispatchNotifications(action);
+
+      return data;
+    });
     if (!data?.error && data?.url) return routerPush(data?.url);
 
     return data;
