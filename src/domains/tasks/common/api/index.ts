0a5976072a15087cbcd5c6dcc28f7c8d38d761e9
davidtaing <adavidtaing@gmail.com>
2023-04-04 01:51:40 +1000

    feat: add config param to getTasksOfCustomerQuery
    to allow getServerProps to pass in an Authentication token

--- src/domains/tasks/common/api/index.ts
+++ src/domains/tasks/common/api/index.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-export const getTasksOfCustomerQuery = () => {
+export const getTasksOfCustomerQuery = (config?: AxiosRequestConfig) => {
   return axios.get(`${process.env.NEXT_PUBLIC_API_URL}api/v1/tasks`, {
+    ...config,
     params: { my_tasks: true },
   });
 };
