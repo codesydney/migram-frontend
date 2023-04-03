787459fb0ca7115d5eebb0c7bc9577056e56fafc
davidtaing <adavidtaing@gmail.com>
2023-04-04 01:42:07 +1000

    feat: auth protect all tasks routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: [
-    "/account/:path*",
-    "/checkout/:path*",
-    "/tasks/createtask.tsx",
-    "/offers",
-  ],
+  matcher: ["/account/:path*", "/checkout/:path*", "/tasks/:path", "/offers"],
 };
