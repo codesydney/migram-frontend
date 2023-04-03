b2d33c81fe60675f5c980317ee96e8dca2b068b9
davidtaing <adavidtaing@gmail.com>
2023-04-04 02:00:51 +1000

    feat: add server side intitial tasks fetch
    on TasksPage
    
    Changes:
    - rename pages/TaskPage to pages/TaskRoute
    - add server-side Tasks fetch via getServerSideProps in TaskRoute
    - define props type for TaskRoute

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,14 +1,31 @@
-import { useSession } from "next-auth/react";
+import { GetServerSideProps } from "next";
+import { getToken } from "next-auth/jwt";
 import { Card, EmptyState, Layout } from "@shopify/polaris";
+
 import {
   PageWithNotifications,
+  useNotifications,
 } from "src/common/features/notifications";
 import { TasksPage as TaskPagePrimitive } from "@Tasks/ViewTasks";
+import { Task } from "@Tasks/common/types";
+import { getTasksOfCustomerQuery } from "@Tasks/common/api";
+import { createNotification } from "src/common/features/notifications/utils";
+import { Notification } from "../../common/features/notifications";
 
-export default function TasksPage() {
-  const { status, data } = useSession();
+export type TaskRouteProps = {
+  isCustomer: boolean;
+  tasks: Task[];
+  error?: Notification;
+};
 
-  if (data?.user.providerId) {
+export default function TasksRoute({
+  isCustomer,
+  tasks,
+  error,
+}: TaskRouteProps) {
+  const { dispatchNotifications } = useNotifications();
+
+  if (!isCustomer) {
     return (
       <PageWithNotifications title="Tasks" fullWidth>
         <Layout>
@@ -26,5 +43,39 @@ export default function TasksPage() {
     );
   }
 
-  return <TaskPagePrimitive status={status} />;
+  if (error) dispatchNotifications({ type: "set", event: error });
+
+  return <TaskPagePrimitive initialTasks={tasks} />;
 }
+
+export const getServerSideProps: GetServerSideProps<TaskRouteProps> = async ({
+  req,
+  res,
+}) => {
+  const token = await getToken({ req });
+  const isCustomer = !!token?.user.customerId;
+
+  if (!token) {
+    return { props: { tasks: [], isCustomer } };
+  }
+
+  try {
+    const tasks = await getTasksOfCustomerQuery({
+      headers: {
+        Authorization: `Bearer ${token?.accessToken}`,
+      },
+    }).then((res) => res.data.data.tasks);
+
+    return { props: { tasks, isCustomer } };
+  } catch (error: any) {
+    const errorNotification = createNotification({
+      isError: true,
+      title: `Failed to fetch tasks. Please refresh the page. If the problem persists, please contact the administrator at ${process.env.ADMIN_EMAIL}`,
+      type: "notification",
+      status: "critical",
+      source: "Mark Task as Completed Failure",
+    });
+
+    return { props: { tasks: [], error: errorNotification, isCustomer } };
+  }
+};
