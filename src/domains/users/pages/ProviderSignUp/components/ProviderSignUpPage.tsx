881cafb55cf6bdab50108e4449bb06874f11df7c
davidtaing <adavidtaing@gmail.com>
2023-04-25 22:04:23 +1000

    refactor: common useSignUpForm logic
    - combine shared implementation for useSignUpForm and useProviderSignUpForm

--- src/domains/users/pages/ProviderSignUp/components/ProviderSignUpPage.tsx
+++ src/domains/users/pages/ProviderSignUp/components/ProviderSignUpPage.tsx
@@ -1,23 +1,98 @@
 import Link from "next/link";
 import { Text } from "@shopify/polaris";
-import { useProviderSignUpForm } from "../hooks";
-import { SignUpForm } from "@Users/common/components/SignUpForm";
+import axios from "axios";
+
+import {
+  SignUpForm,
+  SignUpFormState,
+  useSignUpForm,
+} from "@Users/common/components/SignUpForm";
+import { createUser, signIn } from "@Users/common/api";
+import {
+  useNotifications,
+  createNotification,
+  PageWithNotifications,
+} from "src/common/features/notifications";
+
+/**
+ *
+ * @returns The Provider Sign Up Handler that creates a new Migram User, then redirects them to Stripe for Connect Account Onboarding
+ */
+export const useProviderSignUpHandler = () => {
+  const { dispatchNotifications } = useNotifications();
+
+  const fetchProviderOnboardingUrl = async (userId: string) => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}api/v1/providers`,
+      {
+        UserId: userId,
+      }
+    );
+
+    return response.data.data.accountLink.url;
+  };
+
+  const submitHandler = async (data: SignUpFormState) => {
+    dispatchNotifications({ type: "clear" });
+    const userId = await createUser(data)
+      .then((res) => {
+        const action = {
+          type: "set",
+          event: createNotification({
+            isError: false,
+            title: "Successfully created account. Redirecting you to Stripe",
+            type: "toast",
+            status: "success",
+            source: "Provider Signup Success",
+          }),
+        } as const;
+
+        dispatchNotifications(action);
+
+        return res;
+      })
+      .catch((err) => {
+        const action = {
+          type: "set",
+          event: createNotification({
+            isError: true,
+            title: err.response.data.message,
+            type: "notification",
+            status: "critical",
+            source: "Provider Signup Failure",
+          }),
+        } as const;
+
+        dispatchNotifications(action);
+      });
+
+    await signIn({ email: data.email, password: data.password });
+
+    const providerOnboardingUrl = await fetchProviderOnboardingUrl(userId);
+    window.location.assign(providerOnboardingUrl);
+  };
+
+  return submitHandler;
+};
 
 export const ProviderSignUpPage = () => {
-  const { control, onSubmit } = useProviderSignUpForm();
+  const submitHandler = useProviderSignUpHandler();
+  const { control, onSubmit } = useSignUpForm(submitHandler);
 
   return (
-    <SignUpForm control={control} onSubmit={onSubmit}>
-      <Text as="h2" variant="headingSm">
-        Need help with a Task Instead?{" "}
-        <Link
-          href="/signup"
-          className="Polaris-Link"
-          data-polaris-unstyled="true"
-        >
-          Sign Up as Customer.
-        </Link>
-      </Text>
-    </SignUpForm>
+    <PageWithNotifications title="Sign Up as Provider">
+      <SignUpForm control={control} onSubmit={onSubmit}>
+        <Text as="h2" variant="headingSm">
+          Need help with a Task Instead?{" "}
+          <Link
+            href="/signup"
+            className="Polaris-Link"
+            data-polaris-unstyled="true"
+          >
+            Sign Up as Customer.
+          </Link>
+        </Text>
+      </SignUpForm>
+    </PageWithNotifications>
   );
 };
