7c6ee6e8afd99ae5c52d969e77c8be3e4dc0f2f0
davidtaing <adavidtaing@gmail.com>
2023-05-25 21:16:34 +1000

    wip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs/app-beta";
+import { Header } from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,93 @@
 export default function Home() {
   return (
     <main>
-      Hello World
+      <Tasks />
     </main>
   );
+}
+
+const tasks = [
+  {
+    id: 1,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  {
+    id: 2,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  {
+    id: 3,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  {
+    id: 4,
+    name: 'Leather Long Wallet',
+    color: 'Natural',
+    price: '$75',
+    href: '#',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-04-trending-product-02.jpg',
+    imageAlt: 'Hand stitched, orange leather long wallet.',
+  },
+  // More products...
+]
+
+
+export function Tasks() {
+  return (
+    <div className="bg-white">
+    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="md:flex md:items-center md:justify-between">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Trending products</h2>
+        <a href="#" className="hidden text-sm font-medium text-indigo-600 hover:text-indigo-500 md:block">
+          View More Tasks
+          <span aria-hidden="true"> &rarr;</span>
+        </a>
+      </div>
+
+      <div className="mt-6 grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-0 lg:gap-x-8">
+        {tasks.map((task) => (
+          <div key={task.id} className="group relative">
+            <div className="h-56 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-72 xl:h-80">
+              <img
+                src={task.imageSrc}
+                alt={task.imageAlt}
+                className="h-full w-full object-cover object-center"
+              />
+            </div>
+            <h3 className="mt-4 text-sm text-gray-700">
+              <a href={task.href}>
+                <span className="absolute inset-0" />
+                {task.name}
+              </a>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{task.color}</p>
+            <p className="mt-1 text-sm font-medium text-gray-900">{task.price}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="mt-8 text-sm md:hidden">
+        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+          View More Tasks
+          <span aria-hidden="true"> &rarr;</span>
+        </a>
+      </div>
+    </div>
+  </div>
+  )
 }
