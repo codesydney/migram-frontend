13b8d0bb8fab3f72948f525e7b964fab73f2da5f
davidtaing <adavidtaing@gmail.com>
2023-05-25 22:21:19 +1000

    feat: CreateTaskPage add Due Date field

--- src/app/tasks/create/page.tsx
+++ src/app/tasks/create/page.tsx
@@ -47,27 +47,6 @@ export function CreateTaskForm() {
                       </div>
                     </div>
 
-                    <div className="col-span-full">
-                      <label
-                        htmlFor="about"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                      >
-                        More Details
-                      </label>
-                      <div className="mt-2">
-                        <textarea
-                          id="about"
-                          name="about"
-                          rows={3}
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          defaultValue={""}
-                        />
-                      </div>
-                      <p className="mt-3 text-sm leading-6 text-gray-600">
-                        Write a few sentences about yourself.
-                      </p>
-                    </div>
-
                     <div className="sm:col-span-4">
                       <label
                         htmlFor="category"
@@ -87,6 +66,42 @@ export function CreateTaskForm() {
                           <option>Painting</option>
                           <option>Other</option>
                         </select>
+                      </div>
+                    </div>
+
+                    <div className="col-span-full">
+                      <label
+                        htmlFor="about"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        More Details
+                      </label>
+                      <div className="mt-2">
+                        <textarea
+                          id="about"
+                          name="about"
+                          rows={3}
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                          defaultValue={""}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="sm:col-span-4">
+                      <label
+                        htmlFor="street-address"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Due Date
+                      </label>
+                      <div className="mt-2">
+                        <input
+                          type="date"
+                          name="street-address"
+                          id="street-address"
+                          autoComplete="street-address"
+                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
                       </div>
                     </div>
 
