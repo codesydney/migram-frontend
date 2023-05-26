f4e5845749eac40fd07465bcf003123c138b2344
davidtaing <adavidtaing@gmail.com>
2023-05-26 18:52:39 +1000

    feat: createStripeCustomer handler
    - add getPrimaryEmailAddress util function for Clerk Users
    - save customerId and userId to database when a Stripe customer is created
    - pass Clerk userId as metadata when creating a new Stripe customer

---
+++ src/backend/services/users/utils.ts
@@ -0,0 +1,29 @@
+import { User } from "@clerk/clerk-sdk-node";
+
+export type getPrimaryEmailAddressResult =
+  | {
+      type: "success";
+      email: string;
+    }
+  | {
+      type: "error";
+      error: Error;
+    };
+
+export function getPrimaryEmailAddress(
+  user: User
+): getPrimaryEmailAddressResult {
+  const { primaryEmailAddressId, emailAddresses } = user;
+
+  const primaryEmailAddress = emailAddresses.find(
+    (email) => email.id === primaryEmailAddressId
+  );
+
+  if (!primaryEmailAddress)
+    return {
+      type: "error",
+      error: new Error("Primary Email Address Not Found"),
+    };
+
+  return { type: "success", email: primaryEmailAddress.emailAddress };
+}
--- src/pages/api/customers.ts
+++ src/pages/api/customers.ts
@@ -1,13 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getAuth } from "@clerk/nextjs/server";
 
 import { stripe } from "@/backend/services/payments";
+import { Customer } from "@/backend/data/customers";
+import { getPrimaryEmailAddress } from "@/backend/services/users";
 
 async function createStripeCustomer(req: NextApiRequest, res: NextApiResponse) {
-  const params = { email: "hello@email.com", name: "TODO Remove" };
+  const { user, userId } = getAuth(req);
 
-  const customer = await stripe.customers.create(params);
+  if (!user || !userId) return res.status(401).end("Unauthorized");
 
-  return res.status(200).json({ customer: customer });
+  const emailAddressResult = getPrimaryEmailAddress(user);
+
+  if (emailAddressResult.type === "error")
+    return res.status(500).end("Internal Server Error");
+
+  const params = {
+    email: emailAddressResult.email,
+    metadata: {
+      userId,
+    },
+  };
+
+  const stripeCustomer = await stripe.customers.create(params);
+  const stripeCustomerId = stripeCustomer.id;
+
+  const customerDocument = await Customer.create({
+    userId,
+    customerId: stripeCustomerId,
+  });
+
+  return res.status(200).json({
+    data: customerDocument,
+    message: "Successfully Created Stripe Customer",
+  });
 }
 
 export default async function handler(
