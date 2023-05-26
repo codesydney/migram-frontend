6b52fa9ff623cb07c558480eca4dde16c9716fe6
davidtaing <adavidtaing@gmail.com>
2023-05-27 08:34:48 +1000

    feat(Header): add CustomerBadge and ServiceProviderBadge

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -47,7 +47,8 @@ export function Header() {
             </a>
           ))}
         </div>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-4">
+          <UserTypeBadge type={userRole}></UserTypeBadge>
           <ClerkLoginButtons />
         </div>
       </nav>
@@ -92,6 +93,9 @@ export function Header() {
                 ))}
               </div>
               <div className="py-6">
+                <UserTypeBadge type={userRole}></UserTypeBadge>
+              </div>
+              <div className="py-6">
                 <ClerkLoginButtons />
               </div>
             </div>
@@ -113,5 +117,46 @@ export function ClerkLoginButtons() {
         <SignInButton />
       </SignedOut>
     </>
+  );
+}
+
+export function UserTypeBadge({
+  type,
+}: {
+  type?: "customer" | "service-provider";
+}) {
+  if (type === "customer") return <CustomerBadge />;
+  if (type === "service-provider") return <ServiceProviderBadge />;
+
+  return null;
+}
+
+export function CustomerBadge() {
+  return (
+    <span className="inline-flex items-center gap-x-1.5 rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
+      <svg
+        className="h-1.5 w-1.5 fill-green-500"
+        viewBox="0 0 6 6"
+        aria-hidden="true"
+      >
+        <circle cx={3} cy={3} r={3} />
+      </svg>
+      Customer
+    </span>
+  );
+}
+
+export function ServiceProviderBadge() {
+  return (
+    <span className="inline-flex items-center gap-x-1.5 rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-blue-700">
+      <svg
+        className="h-1.5 w-1.5 fill-blue-500"
+        viewBox="0 0 6 6"
+        aria-hidden="true"
+      >
+        <circle cx={3} cy={3} r={3} />
+      </svg>
+      Service Provider
+    </span>
   );
 }
