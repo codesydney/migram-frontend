0ec44776f15e594a3c8df2f2d428a127f953ad6f
davidtaing <adavidtaing@gmail.com>
2023-05-26 22:05:18 +1000

    feat: add Tasks auth check
    - POST /tasks - check if a user is signed in and is a customer

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Task } from "@/backend/data/tasks";
+import { dbConnect } from "@/backend/services/db";
+import { authenticate } from "@/backend/middlewares/auth";
+import { isUserCustomer } from "@/backend/services/users";
 
 async function getTasks(req: NextApiRequest, res: NextApiResponse) {
   const tasks = await Task.find();
@@ -9,7 +12,23 @@ async function getTasks(req: NextApiRequest, res: NextApiResponse) {
 }
 
 async function createTask(req: NextApiRequest, res: NextApiResponse) {
-  const task = await Task.create({});
+  const authResult = await authenticate(req);
+
+  if (authResult.type === "error")
+    return res.status(authResult.status).json({ message: authResult.message });
+
+  const { user } = authResult;
+
+  const isCustomer = isUserCustomer(user);
+
+  if (!isCustomer) {
+    return res.status(403).json({
+      message: "Forbidden: Only Customer Users can perform this operation",
+    });
+  }
+
+  const payload = req.body;
+  const task = await Task.create(payload);
 
   return res.status(200).json({ data: task });
 }
@@ -18,6 +37,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  await dbConnect();
+
   switch (req.method) {
     case "GET":
       return getTasks(req, res);
