0db6c3fe39d0a7ba80f6c64813a8fe64e9c9d3a8
davidtaing <adavidtaing@gmail.com>
2023-05-26 19:19:16 +1000

    comment out users.ts

--- src/backend/services/users/users.ts
+++ src/backend/services/users/users.ts
@@ -1,68 +1,68 @@
-import pino from "pino";
+// import pino from "pino";
 
-import { getUser, updateUser } from "@/backend/data/users";
+// import { getUser, updateUser } from "@/backend/data/users";
 
-const logger = pino({ name: "Payments Webhook Handler" });
+// const logger = pino({ name: "Payments Webhook Handler" });
 
-if (!process.env.NEXT_PUBLIC_CALLS_INCREMENT_AMOUNT)
-  throw new Error("NEXT_PUBLIC_CALLS_INCREMENT_AMOUNT not set");
+// if (!process.env.NEXT_PUBLIC_CALLS_INCREMENT_AMOUNT)
+//   throw new Error("NEXT_PUBLIC_CALLS_INCREMENT_AMOUNT not set");
 
-const CALLS_INCREMENT_AMOUNT = parseInt(
-  process.env.NEXT_PUBLIC_CALLS_INCREMENT_AMOUNT
-);
+// const CALLS_INCREMENT_AMOUNT = parseInt(
+//   process.env.NEXT_PUBLIC_CALLS_INCREMENT_AMOUNT
+// );
 
-/**
- * Stripped down version of the Clerk SDK's User type,
- * so we don't expose sensitive user data.
- */
-export type FilteredUpdateUserCalls = {
-  id: string;
-  email: string;
-  callsRemaining: number;
-};
+// /**
+//  * Stripped down version of the Clerk SDK's User type,
+//  * so we don't expose sensitive user data.
+//  */
+// export type FilteredUpdateUserCalls = {
+//   id: string;
+//   email: string;
+//   callsRemaining: number;
+// };
 
-export type UpdateUserCallsResult =
-  | {
-      type: "success";
-      user: FilteredUpdateUserCalls;
-    }
-  | {
-      type: "error";
-      status: number;
-      error: Error;
-    };
+// export type UpdateUserCallsResult =
+//   | {
+//       type: "success";
+//       user: FilteredUpdateUserCalls;
+//     }
+//   | {
+//       type: "error";
+//       status: number;
+//       error: Error;
+//     };
 
-export async function updateUserCalls(
-  userId: string
-): Promise<UpdateUserCallsResult> {
-  const existingUser = await getUser(userId);
-  if (!existingUser) {
-    const message = "User not found";
-    logger.error({ userId }, message);
+// export async function updateUserCalls(
+//   userId: string
+// ): Promise<UpdateUserCallsResult> {
+//   const existingUser = await getUser(userId);
+//   if (!existingUser) {
+//     const message = "User not found";
+//     logger.error({ userId }, message);
 
-    return { type: "error", status: 404, error: new Error(message) };
-  }
+//     return { type: "error", status: 404, error: new Error(message) };
+//   }
 
-  const callsRemaining = existingUser.publicMetadata.callsRemaining as number;
-  const newBalance = callsRemaining + CALLS_INCREMENT_AMOUNT;
+//   const callsRemaining = existingUser.publicMetadata.callsRemaining as number;
+//   const newBalance = callsRemaining + CALLS_INCREMENT_AMOUNT;
 
-  const user = await updateUser(userId, {
-    publicMetadata: {
-      callsRemaining: newBalance,
-    },
-  });
+//   const user = await updateUser(userId, {
+//     publicMetadata: {
+//       callsRemaining: newBalance,
+//     },
+//   });
 
-  const filteredUser: FilteredUpdateUserCalls = {
-    id: user.id,
-    email: user.emailAddresses[0].emailAddress,
-    callsRemaining: user.publicMetadata.callsRemaining as number,
-  };
+//   const filteredUser: FilteredUpdateUserCalls = {
+//     id: user.id,
+//     email: user.emailAddresses[0].emailAddress,
+//     callsRemaining: user.publicMetadata.callsRemaining as number,
+//   };
 
-  const result = {
-    type: "success" as const,
-    user: filteredUser,
-  };
-  logger.info({ result }, "User Successfully Updated");
+//   const result = {
+//     type: "success" as const,
+//     user: filteredUser,
+//   };
+//   logger.info({ result }, "User Successfully Updated");
 
-  return result;
-}
+//   return result;
+// }
