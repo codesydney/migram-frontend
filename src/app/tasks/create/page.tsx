3b4dc0e3d184438796134ed276f606d18afec3fa
davidtaing <adavidtaing@gmail.com>
2023-05-26 23:46:58 +1000

    refactor(CreateTaskPage): consolidate components

--- src/app/tasks/create/page.tsx
+++ src/app/tasks/create/page.tsx
@@ -1,10 +1,4 @@
-import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
-
 export default function CreateTaskPage() {
-  return <CreateTaskForm />;
-}
-
-function CreateTaskForm() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
