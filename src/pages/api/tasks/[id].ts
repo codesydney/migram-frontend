e41b2c9019575710124058c3b4eb207a65faa64e
davidtaing <adavidtaing@gmail.com>
2023-05-26 22:32:09 +1000

    feat: add auth checks when updating a Task

--- src/pages/api/tasks/[id].ts
+++ src/pages/api/tasks/[id].ts
@@ -1,7 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 import { Task } from "@/backend/data/tasks";
+import { authenticate } from "@/backend/middlewares/auth";
 import { dbConnect } from "@/backend/services/db";
+import { isUserCustomer } from "@/backend/services/users";
+import { CustomerMetadata } from "@/backend/services/users/types";
 
 async function getTaskById(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
@@ -11,12 +14,37 @@ async function getTaskById(req: NextApiRequest, res: NextApiResponse) {
 }
 
 async function updateTask(req: NextApiRequest, res: NextApiResponse) {
+  const authResult = await authenticate(req);
+
+  if (authResult.type === "error")
+    return res.status(authResult.status).json({ message: authResult.message });
+
+  const { user } = authResult;
+
+  const isCustomer = isUserCustomer(user);
+  if (!isCustomer)
+    return res.status(403).json({
+      message: "Forbidden: Only Customers may perform this action.",
+    });
+
   const { id } = req.query;
+  const task = await Task.findById({ id });
+
+  const userMetadata = user.publicMetadata as CustomerMetadata;
+  const customerId = userMetadata.customerId;
+  const isTaskOwner = customerId === task.customerId;
+
+  if (!isTaskOwner) {
+    return res.status(403).json({
+      message: "Forbidden: You do not have access to this resource.",
+    });
+  }
 
   const payload = req.body;
-  const task = await Task.updateOne({ id }, payload);
+  task.set(payload);
+  const updatedTask = await task.save();
 
-  return res.status(200).json({ data: task });
+  return res.status(200).json({ data: updatedTask });
 }
 
 export default async function handler(
