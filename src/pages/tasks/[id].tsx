306bd5ff93be7261a8146fdb1bdb1dc1fa543790
davidtaing <adavidtaing@gmail.com>
2023-05-27 18:11:03 +1000

    feat(TaskOffersList): remove add button

--- src/pages/tasks/[id].tsx
+++ src/pages/tasks/[id].tsx
@@ -123,14 +123,6 @@ export function TaskOffersList({ taskId }: { taskId: string }) {
             A list offers on your task
           </p>
         </div>
-        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-          <button
-            type="button"
-            className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Add user
-          </button>
-        </div>
       </div>
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
