4a40498498fa2f60020726e9b59f486dfe4a16e7
davidtaing <adavidtaing@gmail.com>
2023-05-28 18:04:21 +1000

    feat(TaskOffersList): render offer data in table
    instead of placeholder data

--- src/pages/tasks/[id].tsx
+++ src/pages/tasks/[id].tsx
@@ -172,7 +172,7 @@ export function TaskOffersList({ taskId }: { taskId: string }) {
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
-                    Title
+                    Amount
                   </th>
                   <th
                     scope="col"
@@ -184,56 +184,49 @@ export function TaskOffersList({ taskId }: { taskId: string }) {
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                   >
-                    Role
+                    Message
                   </th>
-                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span className="sr-only">Edit</span>
+
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Action
                   </th>
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
-                {people.map((person) => (
-                  <tr key={person.email}>
+                {offers.map((offer) => (
+                  <tr key={offer._id}>
                     <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
                       <div className="flex items-center">
                         <div className="h-11 w-11 flex-shrink-0">
                           <img
                             className="h-11 w-11 rounded-full"
-                            src={person.image}
+                            src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                             alt=""
                           />
                         </div>
                         <div className="ml-4">
                           <div className="font-medium text-gray-900">
-                            {person.name}
+                            {"David Taing"}
                           </div>
-                          <div className="mt-1 text-gray-500">
-                            {person.email}
-                          </div>
+                          {/* <div className="mt-1 text-gray-500">
+                            {"adavidtaing@gmail.com"}
+                          </div> */}
                         </div>
                       </div>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      <div className="text-gray-900">{person.title}</div>
-                      <div className="mt-1 text-gray-500">
-                        {person.department}
-                      </div>
+                    <td className="whitespace-nowrap px-3 py-5 text-md text-gray-900">
+                      ${offer.amount}
                     </td>
                     <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
                       <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                        Active
+                        {offer.status}
                       </span>
                     </td>
-                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-                      {person.role}
-                    </td>
-                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      <a
-                        href="#"
-                        className="text-indigo-600 hover:text-indigo-900"
-                      >
-                        Edit<span className="sr-only">, {person.name}</span>
-                      </a>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-900">
+                      {offer.message}
                     </td>
                   </tr>
                 ))}
