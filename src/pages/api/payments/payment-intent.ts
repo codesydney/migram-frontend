aa58ad0427a2af93d291f089b0f2a15e260f3fdb
davidtaing <adavidtaing@gmail.com>
2023-05-30 23:41:01 +1000

    feat: add create paymentIntent endpoint

---
+++ src/pages/api/payments/payment-intent.ts
@@ -0,0 +1,52 @@
+import to from "await-to-js";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { TaskModel } from "@/backend/data/tasks";
+import { stripe } from "@/backend/services/payments/stripe";
+import { OfferModel } from "@/backend/data/offers";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.body !== "POST")
+    return res.status(400).json({ message: "Invalid request method" });
+
+  const taskId = req.query.taskId as string;
+
+  const task = await TaskModel.findOne({ _id: taskId });
+  if (!task) return res.status(404).json({ message: "Task not found" });
+
+  if (task.status !== "Completed")
+    return res.status(400).json({
+      message: "Task cannot be checked out when the status is not 'Completed'",
+    });
+
+  const acceptedOffer = await OfferModel.findOne({ _id: task.acceptedOffer });
+
+  if (!task.acceptedOffer || !acceptedOffer)
+    return res.status(400).json({
+      message:
+        "Task cannot be checked out when an offer has not been accepted yet.",
+    });
+
+  const [err, paymentIntent] = await to(
+    stripe.paymentIntents.create({
+      amount: acceptedOffer.amount * 100,
+      currency: "aud",
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    })
+  );
+
+  if (err || !paymentIntent) {
+    return res
+      .status(500)
+      .json({ message: "failed to create a new payment intent" });
+  }
+
+  return res.status(200).json({
+    clientSecret: paymentIntent.client_secret,
+  });
+}
