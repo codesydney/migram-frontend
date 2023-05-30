3dd6754341106a98690ce24692aebe4df9036e84
davidtaing <adavidtaing@gmail.com>
2023-05-30 23:54:10 +1000

    feat: add CheckoutPage with minimal implementation

---
+++ src/pages/checkout/[taskId].tsx
@@ -0,0 +1,318 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { StateSchema, Task } from "@/types/schemas/Task";
+import { Disclosure } from "@headlessui/react";
+
+const subtotal = "$108.00";
+const discount = { code: "CHEAPSKATE", amount: "$16.00" };
+const taxes = "$9.92";
+const shipping = "$8.00";
+const total = "$141.92";
+const products = [
+  {
+    id: 1,
+    name: "Mountain Mist Artwork Tee",
+    href: "#",
+    price: "$36.00",
+    color: "Birch",
+    size: "L",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/checkout-form-04-product-01.jpg",
+    imageAlt:
+      "Off-white t-shirt with circular dot illustration on the front of mountain ridges that fade.",
+  },
+  // More products...
+];
+
+export default function CheckoutForm() {
+  return (
+    <>
+      <main className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 lg:flex lg:min-h-full lg:flex-row-reverse lg:overflow-hidden">
+        <h1 className="sr-only">Checkout</h1>
+
+        {/* Mobile order summary */}
+        <section
+          aria-labelledby="order-heading"
+          className="bg-gray-50 px-4 py-6 sm:px-6 lg:hidden"
+        >
+          <Disclosure as="div" className="mx-auto max-w-lg">
+            {({ open }) => (
+              <>
+                <div className="flex items-center justify-between">
+                  <h2
+                    id="order-heading"
+                    className="text-lg font-medium text-gray-900"
+                  >
+                    Your Order
+                  </h2>
+                  <Disclosure.Button className="font-medium text-indigo-600 hover:text-indigo-500">
+                    {open ? (
+                      <span>Hide full summary</span>
+                    ) : (
+                      <span>Show full summary</span>
+                    )}
+                  </Disclosure.Button>
+                </div>
+
+                <Disclosure.Panel>
+                  <ul
+                    role="list"
+                    className="divide-y divide-gray-200 border-b border-gray-200"
+                  >
+                    {products.map((product) => (
+                      <li key={product.id} className="flex space-x-6 py-6">
+                        <img
+                          src={product.imageSrc}
+                          alt={product.imageAlt}
+                          className="h-40 w-40 flex-none rounded-md bg-gray-200 object-cover object-center"
+                        />
+                        <div className="flex flex-col justify-between space-y-4">
+                          <div className="space-y-1 text-sm font-medium">
+                            <h3 className="text-gray-900">{product.name}</h3>
+                            <p className="text-gray-900">{product.price}</p>
+                            <p className="text-gray-500">{product.color}</p>
+                            <p className="text-gray-500">{product.size}</p>
+                          </div>
+                          <div className="flex space-x-4">
+                            <button
+                              type="button"
+                              className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                            >
+                              Edit
+                            </button>
+                            <div className="flex border-l border-gray-300 pl-4">
+                              <button
+                                type="button"
+                                className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                              >
+                                Remove
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <dl className="mt-10 space-y-6 text-sm font-medium text-gray-500">
+                    <div className="flex justify-between">
+                      <dt>Subtotal</dt>
+                      <dd className="text-gray-900">{subtotal}</dd>
+                    </div>
+                    <div className="flex justify-between">
+                      <dt>Taxes</dt>
+                      <dd className="text-gray-900">{taxes}</dd>
+                    </div>
+                    <div className="flex justify-between">
+                      <dt>Shipping</dt>
+                      <dd className="text-gray-900">{shipping}</dd>
+                    </div>
+                  </dl>
+                </Disclosure.Panel>
+
+                <p className="mt-6 flex items-center justify-between border-t border-gray-200 pt-6 text-sm font-medium text-gray-900">
+                  <span className="text-base">Total</span>
+                  <span className="text-base">{total}</span>
+                </p>
+              </>
+            )}
+          </Disclosure>
+        </section>
+
+        {/* Order summary */}
+        <section
+          aria-labelledby="summary-heading"
+          className="hidden w-full max-w-md flex-col bg-gray-50 lg:flex"
+        >
+          <h2 id="summary-heading" className="sr-only">
+            Order summary
+          </h2>
+
+          <ul
+            role="list"
+            className="flex-auto divide-y divide-gray-200 overflow-y-auto px-6"
+          >
+            {products.map((product) => (
+              <li key={product.id} className="flex space-x-6 py-6">
+                <img
+                  src={product.imageSrc}
+                  alt={product.imageAlt}
+                  className="h-40 w-40 flex-none rounded-md bg-gray-200 object-cover object-center"
+                />
+                <div className="flex flex-col justify-between space-y-4">
+                  <div className="space-y-1 text-sm font-medium">
+                    <h3 className="text-gray-900">{product.name}</h3>
+                    <p className="text-gray-900">{product.price}</p>
+                    <p className="text-gray-500">{product.color}</p>
+                    <p className="text-gray-500">{product.size}</p>
+                  </div>
+                  <div className="flex space-x-4">
+                    <button
+                      type="button"
+                      className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      Edit
+                    </button>
+                    <div className="flex border-l border-gray-300 pl-4">
+                      <button
+                        type="button"
+                        className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          <div className="sticky bottom-0 flex-none border-t border-gray-200 bg-gray-50 p-6">
+            <dl className="space-y-6 text-sm font-medium text-gray-500">
+              <div className="flex justify-between">
+                <dt>Subtotal</dt>
+                <dd className="text-gray-900">{subtotal}</dd>
+              </div>
+              <div className="flex justify-between">
+                <dt>Taxes</dt>
+                <dd className="text-gray-900">{taxes}</dd>
+              </div>
+              <div className="flex items-center justify-between border-t border-gray-200 pt-6 text-gray-900">
+                <dt>Total</dt>
+                <dd className="text-base">{total}</dd>
+              </div>
+            </dl>
+          </div>
+        </section>
+
+        {/* Checkout form */}
+        <section
+          aria-labelledby="payment-heading"
+          className="flex-auto overflow-y-auto px-4 pb-16 pt-12 sm:px-6 sm:pt-16 lg:px-8 lg:pb-24 lg:pt-0"
+        >
+          <h2 id="payment-heading" className="sr-only">
+            Payment and shipping details
+          </h2>
+
+          <div className="mx-auto max-w-lg lg:pt-16">
+            <form className="mt-6">
+              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="col-span-full">
+                  <label
+                    htmlFor="street-address"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Street address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      id="street-address"
+                      autoComplete="street-address"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      // {...register("location.streetAddress")}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {/* {errors.location?.streetAddress?.message as string} */}
+                  </p>
+                </div>
+
+                <div className="sm:col-span-2 sm:col-start-1">
+                  <label
+                    htmlFor="city"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    City
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      id="city"
+                      autoComplete="address-level2"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      // {...register("location.city")}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {/* {errors.location?.city?.message as string} */}
+                  </p>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="state"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    State
+                  </label>
+                  <div className="mt-2">
+                    <select
+                      id="state"
+                      autoComplete="state"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                      // {...register("location.state")}
+                    >
+                      {StateSchema.options.map((state) => (
+                        <option value={state} key={state}>
+                          {state}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {/* {errors.location?.state?.message as string} */}
+                  </p>
+                </div>
+
+                <div className="sm:col-span-2">
+                  <label
+                    htmlFor="postal-code"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Postcode
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      id="postal-code"
+                      autoComplete="postal-code"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      // {...register("location.postal_code")}
+                    />
+                  </div>
+                  <p className="mt-2 text-sm text-red-600" id="email-error">
+                    {/* {errors.location?.postal_code?.message as string} */}
+                  </p>
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="mt-6 w-full rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                Pay {total}
+              </button>
+
+              <p className="mt-6 flex justify-center text-sm font-medium text-gray-500">
+                Payment via Stripe
+              </p>
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
