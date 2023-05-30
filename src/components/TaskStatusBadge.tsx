c60f77d650c057779b286fc71e680ab350058bdc
davidtaing <adavidtaing@gmail.com>
2023-05-30 22:14:47 +1000

    refactor: TaskStatusBadge classNames via clsx

--- src/components/TaskStatusBadge.tsx
+++ src/components/TaskStatusBadge.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 import { TaskStatus } from "@/types/schemas/Task";
 
 export type TaskStatusBadgeProps = {
@@ -5,26 +7,21 @@ export type TaskStatusBadgeProps = {
 };
 
 export function TaskStatusBadge({ status }: TaskStatusBadgeProps) {
+  let className = "";
+  const baseClasses =
+    "inline-flex items-center rounded-md px-2 py-1 text-xs font-medium";
+
   switch (status) {
     case "Open":
-      return (
-        <span className="inline-flex items-center rounded-md bg-blue-100 px-2 py-1 text-xs font-medium text-blue-700">
-          {status}
-        </span>
-      );
+      className = clsx(baseClasses, "bg-blue-100 text-blue-700");
+      break;
     case "In Progress":
-      return (
-        <span className="inline-flex items-center rounded-md bg-green-100 px-2 py-1 text-xs font-medium text-green-700">
-          {status}
-        </span>
-      );
+      className = clsx(baseClasses, "bg-green-100 text-green-700");
+      break;
     case "Completed":
-      return (
-        <span className="inline-flex items-center rounded-md bg-pink-100 px-2 py-1 text-xs font-medium text-pink-700">
-          {status}
-        </span>
-      );
-    default:
-      return <></>;
+      className = clsx(baseClasses, "bg-pink-100 text-pink-700");
+      break;
   }
+
+  return <span className={className}>{status}</span>;
 }
