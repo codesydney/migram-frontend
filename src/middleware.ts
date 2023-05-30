8a50f188b51d1ad778b6955fb3c48f1065252910
davidtaing <adavidtaing@gmail.com>
2023-05-30 20:20:38 +1000

    fix: clerk redirect on api requests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/tasks(.*)"],
+  publicRoutes: ["/", "/listings", "/(api)(.*)"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api)(.*)"],
 };
