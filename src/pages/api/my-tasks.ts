4a825565d40526184ddf7f0ac37e544e14d154f2
davidtaing <adavidtaing@gmail.com>
2023-06-01 23:13:26 +1000

    feat: add validation if user is a Customer
    for the GET /api/my-tasks endpoint

--- src/pages/api/my-tasks.ts
+++ src/pages/api/my-tasks.ts
@@ -4,6 +4,8 @@ import pino from "pino";
 import { TaskModel } from "@/backend/data/tasks";
 import { dbConnect } from "@/backend/services/db";
 import { OfferModel } from "@/backend/data/offers";
+import { authenticate } from "@/backend/middlewares/auth";
+import { UserMetadata } from "@/backend/services/users/types";
 
 const logger = pino({ name: "GET /api/my-tasks" });
 
@@ -14,10 +16,22 @@ export default async function handler(
   if (req.method !== "GET")
     return res.status(405).json({ message: "Method Not Supported" });
 
+  const authResult = await authenticate(req);
+
+  if (authResult.type === "error")
+    return res.status(authResult.status).json({ message: authResult.message });
+
+  const userMetadata = authResult.user.publicMetadata as UserMetadata;
+
+  if (userMetadata.role !== "customer")
+    return res
+      .status(403)
+      .json({ message: "You are not authorized to access this endpoint." });
+
   await dbConnect();
 
   const tasks = await TaskModel.find({
-    userId: "user_2QHeWJyb22UhmMXAvbRjSmyCjHg",
+    userId: authResult.userId,
   })
     .populate({
       path: "acceptedOffer",
