5819b9314e2e411235e60e646c613cfbe06d98ac
davidtaing <adavidtaing@gmail.com>
2023-06-01 23:23:54 +1000

    refactor(MyTasks): remove SignedIn component
    as page is protected

--- src/pages/my-tasks.tsx
+++ src/pages/my-tasks.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-import { SignedIn } from "@clerk/nextjs";
+import axios from "axios";
 
 import { Task } from "@/types/schemas/Task";
 import { TaskStatusBadge } from "@/components/TaskStatusBadge";
-import axios from "axios";
 import { Offer } from "@/types/schemas/Offer";
 import { MakePaymentButton } from "@/components/MakePaymentButton";
 import { PaymentStatusBadge } from "@/components/PaymentStatusBadge";
@@ -33,16 +32,14 @@ export default function MyTasksPage() {
           <h2 className="inline-block text-2xl font-bold tracking-tight text-gray-900">
             My Tasks
           </h2>
-          <SignedIn>
-            <a href="/tasks/create">
-              <button
-                type="button"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Create Task
-              </button>
-            </a>
-          </SignedIn>
+          <a href="/tasks/create">
+            <button
+              type="button"
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Create Task
+            </button>
+          </a>
         </div>
         <div className="mt-8 flow-root">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
